55bce30974d1088e501c538e470d1a0392a3b25a
Jordan Demmouche <jordand75020@hotmail.fr>
2020-10-14 22:34:35 +0200

    Add dependent: :destroy on models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :addresses
-  has_many :subscriptions, through: :address
+  has_many :addresses, dependent: :destroy
+  has_many :subscriptions, through: :addresses
 end
