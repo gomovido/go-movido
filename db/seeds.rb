a589bc12dfffa639f4ba792cff08152ece1d268d
Jordan Demmouche <jordand75020@hotmail.fr>
2020-10-14 22:40:14 +0200

    Users & Addresses in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    already_moved: [true, false].sample,
+    moving_date: Faker::Date.between(from: '2019-11-18', to: '2021-09-25'),
+    phone: "+33#{Faker::Number.number(digits: 9)}",
+    city: Faker::Address.city,
+    housed: [true, false].sample,
+    address: Faker::Address.street_address,
+    password: 'movido123456',
+    password_confirmation: 'movido123456'
+  )
+  user.save
+  2.times do
+    Address.create(
+      user: user,
+      country: Faker::Address.country,
+      city: Faker::Address.city,
+      zipcode: Faker::Address.zip_code,
+      street: Faker::Address.street_name,
+      street_number: Faker::Address.building_number,
+      floor: Faker::Number.number(digits: 8),
+      internet_status: [true, false].sample,
+      phone: "+33#{Faker::Number.number(digits: 9)}",
+      mobile_phone: "+33#{Faker::Number.number(digits: 9)}",
+      building: Faker::Address.building_number,
+      stairs: ['A', 'B', 'C', 'D'].sample,
+      door: ['Gauche', 'Face', 'Droite'].sample,
+      gate_code: Faker::Number.number(digits: 4)
+    )
+  end
+  p "#{user.first_name} created with #{user.addresses.count} addresses" if user and user.addresses
+end
+
+
