d8fa49a74495df59b39796e2d4f61ab72210cf68
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-15 17:17:35 +0200

    Fix seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   phony_normalize :phone, default_country_code: 'FR'
   validates_plausible_phone :phone, presence: true
-  validates :address, presence: true, if: -> { :housed == false }
+  validates :address, presence: true, if: -> { :not_housed == false }
   validates :moving_date, presence: true, if: -> { :alread_moved == false }
 
 end
