494ba38c25a8d5708cd71f6e5b9947d1f586468e
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-15 17:11:41 +0200

    Changed columns names (housed & logo)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_134036) do
+ActiveRecord::Schema.define(version: 2020_10_15_151030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_134036) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "rating"
     t.integer "reviews"
-    t.string "logo"
+    t.string "logo_url"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 2020_10_15_134036) do
     t.date "moving_date"
     t.string "phone"
     t.string "city"
-    t.boolean "housed"
+    t.boolean "not_housed"
     t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
