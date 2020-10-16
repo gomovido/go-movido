90a4b795210e950b2c4e392ff82f7865396751d9
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-16 18:26:54 +0200

    Modal ready

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -1,9 +1,10 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ "button", "title" ]
+  static targets = [ "button", "title"]
 
-  transform() {
-    this.buttonTarget.classList.contains('cross') ? this.buttonTarget.classList.remove('cross') : this.buttonTarget.classList.add('cross');
+  toggle() {
+    this.buttonTarget.classList.toggle('cross');
+    $('#exampleModal').modal('toggle');
   }
 }
