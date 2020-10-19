77405b857e269403184dbd5503b72a5804200910
Jordan Demmouche <jordand75020@hotmail.fr>
2020-10-19 14:09:26 +0200

    phone input flag selector

--- app/javascript/controllers/registration_controller.js
+++ app/javascript/controllers/registration_controller.js
@@ -1,10 +1,14 @@
 import { Controller } from "stimulus";
 import datepicker from '../packs/datepicker';
+import intlTelInput from 'intl-tel-input';
+
+
 
 export default class extends Controller {
-  static targets = [ "date" ]
+  static targets = [ "date", "phone" ]
 
   connect() {
     datepicker(this.dateTarget);
+    intlTelInput(this.phoneTarget, {});
   }
 }
