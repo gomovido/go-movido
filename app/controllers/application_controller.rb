dbe6e6c11e8bb980e1683b595aab7789ced2ae87
Jordan Demmouche <jordand75020@hotmail.fr>
2020-10-19 18:46:53 +0200

    User are able to create account and sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   private
+
+  def after_sign_in_path_for(resource)
+    dashboard_index_path
+  end
 
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
@@ -11,4 +17,21 @@ class ApplicationController < ActionController::Base
   def default_url_options
     { locale: I18n.locale }
   end
+
+ protected
+
+ def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+      keys: [
+        :email, :first_name, :last_name,
+        :already_moved, :moving_date, :phone,
+        :city, :not_housed, :address
+      ]
+    )
+    devise_parameter_sanitizer.permit(:sign_in,
+      keys: [
+        :email, :password, :password_confirmation
+      ]
+    )
+ end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,5 @@
 class SessionsController < Devise::SessionsController
-
   after_action :prepare_intercom_shutdown, only: [:destroy]
-
-  # Your logic here
 
   protected
   def prepare_intercom_shutdown
