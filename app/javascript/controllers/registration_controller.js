6f0b9e245f4e3ca4b7f575623e6c419d0b794ab2
Jordan Demmouche <jordand75020@hotmail.fr>
2020-10-20 00:12:25 +0200

    Indicator are now visible in input

--- app/javascript/controllers/registration_controller.js
+++ app/javascript/controllers/registration_controller.js
@@ -9,6 +9,16 @@ export default class extends Controller {
 
   connect() {
     datepicker(this.dateTarget);
-    intlTelInput(this.phoneTarget, {});
+    intlTelInput(this.phoneTarget, {
+      preferredCountries: ['fr']
+    });
+    this.phoneTarget.value = `+${document.querySelector('.iti__active').getAttribute('data-dial-code')}`;
+    const countries = document.querySelectorAll('.iti__country');
+    countries.forEach((country) => {
+      country.addEventListener('click', () => {
+        this.phoneTarget.value = `+${country.getAttribute('data-dial-code')}`;
+      })
+    })
+
   }
 }
