811d5bc975b7290f33aca249196656c0b5550a98
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-22 14:02:14 +0200

    Logout fixed

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,9 @@ class SessionsController < Devise::SessionsController
   after_action :prepare_intercom_shutdown, only: [:destroy]
 
   protected
+
   def prepare_intercom_shutdown
     IntercomRails::ShutdownHelper::intercom_shutdown_helper(cookies)
   end
+
 end
