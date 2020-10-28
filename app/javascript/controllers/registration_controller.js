a621dcab2ad86b70267ae42a66ae66b1bd065169
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-28 10:19:43 +0100

    Registration form js & validation

--- app/javascript/controllers/registration_controller.js
+++ app/javascript/controllers/registration_controller.js
@@ -4,11 +4,16 @@ import displayAddressAutocomplete from '../packs/algolia';
 
 
 export default class extends Controller {
-  static targets = [ "date", "phone", "dateInput", "addressInput" ]
+  static targets = [ "date", "phone", "dateInput", "addressInput", "checkboxAddress", "checkboxDate" ]
 
   connect() {
     phoneInput(this.phoneTarget);
-    displayAddressAutocomplete(this.addressInputTarget)
+    displayAddressAutocomplete(this.addressInputTarget);
+    this.phoneTarget.value = this.phoneTarget.getAttribute("value");
+    this.addressInputTarget.value = this.addressInputTarget.getAttribute("value");
+    this.checkboxAddressTarget.checked ? this.addressInputTarget.classList.add('d-none') : this.addressInputTarget.classList.remove('d-none');
+    this.checkboxDateTarget.checked ? this.dateInputTarget.classList.add('d-none') : this.dateInputTarget.classList.remove('d-none');
+
   }
 
   toggleAddress() {
