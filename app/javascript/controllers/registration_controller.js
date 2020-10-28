66100624fde24bc740332baedb8483c14b311763
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-28 10:26:44 +0100

    Hiding algolia div

--- app/javascript/controllers/registration_controller.js
+++ app/javascript/controllers/registration_controller.js
@@ -11,13 +11,14 @@ export default class extends Controller {
     displayAddressAutocomplete(this.addressInputTarget);
     this.phoneTarget.value = this.phoneTarget.getAttribute("value");
     this.addressInputTarget.value = this.addressInputTarget.getAttribute("value");
-    this.checkboxAddressTarget.checked ? this.addressInputTarget.classList.add('d-none') : this.addressInputTarget.classList.remove('d-none');
+    this.checkboxAddressTarget.checked ? document.querySelector('.user_addresses_street').classList.add('d-none') : document.querySelector('.user_addresses_street').classList.remove('d-none');
     this.checkboxDateTarget.checked ? this.dateInputTarget.classList.add('d-none') : this.dateInputTarget.classList.remove('d-none');
 
   }
 
   toggleAddress() {
-    this.addressInputTarget.classList.toggle('d-none');
+    document.querySelector('.user_addresses_street').classList.toggle('d-none');
+    this.addressInputTarget.value = '';
   }
 
   toggleDate() {
