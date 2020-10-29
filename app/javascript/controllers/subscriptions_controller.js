4d8ea2473920d182e352c80a48642d1638b97c9c
Jordan Demmouche <jordand75020@hotmail.fr>
2020-10-29 19:41:04 +0100

    billing form design

--- app/javascript/controllers/subscriptions_controller.js
+++ app/javascript/controllers/subscriptions_controller.js
@@ -4,18 +4,5 @@ const places = require('places.js');
 export default class extends Controller {
   static targets = [ "addressForm", "addressSummary", "addressInput"]
 
-  connect() {
-    const placesAutocomplete = places({
-      appId: 'pl32PKK41FYV',
-      apiKey: '40d7e2e6a30185453dfe6ae9ba07433f',
-      container: this.addressInputTarget
-    });
-  }
-
-
-  toggleAddressForm(event) {
-    this.addressSummaryTarget.classList.remove('d-flex');
-    this.addressSummaryTarget.classList.add('d-none');
-    this.addressFormTarget.classList.toggle('d-none');
-  }
+  connect() {}
 }
