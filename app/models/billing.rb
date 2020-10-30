ca907200748e0ef2baa9aa5d69d9c7e6d60e6f72
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-30 15:26:50 +0100

    Cleaning

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -4,4 +4,8 @@ class Billing < ApplicationRecord
   validates_presence_of :address, :first_name, :last_name, :bic, :iban
   validates_with IbanValidator
   accepts_nested_attributes_for :subscription
+
+  def iban_prettify
+    IBANTools::IBAN.new(self.iban).prettify
+  end
 end
