d5512dbe41ac9f758946128ca1998bf5708fa051
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-30 15:03:10 +0100

    Iban valdiation

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -2,5 +2,6 @@ class Billing < ApplicationRecord
   has_one :subscription, dependent: :destroy
   belongs_to :user
   validates_presence_of :address, :first_name, :last_name, :bic, :iban
+  validates_with IbanValidator
   accepts_nested_attributes_for :subscription
 end
