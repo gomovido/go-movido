c029b003e4a44c1258e01b16c808d789aa0942bc
Yoni Touboul <yonitouboul@hotmail.com>
2020-10-30 15:28:54 +0100

    Deleted useless files

--- app/javascript/controllers/subscriptions_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "stimulus";
-const places = require('places.js');
-
-export default class extends Controller {
-  static targets = [ "addressForm", "addressSummary", "addressInput"]
-
-  connect() {}
-}
