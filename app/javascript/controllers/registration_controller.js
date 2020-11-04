b873f8ddaf5302861d4207543846bb3beef66442
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-04 11:51:48 +0100

    Cleaned forms

--- app/javascript/controllers/registration_controller.js
+++ app/javascript/controllers/registration_controller.js
@@ -9,7 +9,6 @@ export default class extends Controller {
   connect() {
     phoneInput(this.phoneTarget);
     displayAddressAutocomplete(this.addressInputTarget);
-    this.addressInputTarget.value = this.addressInputTarget.getAttribute("value");
     this.checkboxAddressTarget.checked ? document.querySelector('.user_addresses_street').classList.add('d-none') : document.querySelector('.user_addresses_street').classList.remove('d-none');
 
   }
