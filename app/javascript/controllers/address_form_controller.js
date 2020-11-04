a1a3bb48596fc759d21c14349c70ecde5d30eb3d
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-04 11:01:33 +0100

    Rolled back on phone & mobile phone subscriptions

--- app/javascript/controllers/address_form_controller.js
+++ app/javascript/controllers/address_form_controller.js
@@ -3,13 +3,9 @@ import phoneInput from '../packs/phone-input';
 
 
 export default class extends ApplicationController {
-  static targets = ['phone', 'submit', 'input', 'mobilePhone', 'phoneField']
+  static targets = ['phone', 'submit', 'input', 'mobilePhone']
 
   connect() {
     phoneInput(this.mobilePhoneTarget);
-  }
-
-  toggleMobile() {
-    this.phoneFieldTarget.classList.toggle('d-none');
   }
 }
