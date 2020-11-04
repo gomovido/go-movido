1bc18b830b1e001b8540158bcbeed45402bd1a53
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-04 16:12:57 +0100

    Routes, workflow, controllers

--- app/controllers/billings_controller.rb
+++ app/controllers/billings_controller.rb
@@ -1,14 +1,4 @@
 class BillingsController < ApplicationController
-  before_action :set_address, only: [:new, :create]
-  before_action :set_product, only: [:new, :create]
-
-
-  def new
-    @subscription = Subscription.find(params[:subscription_id])
-    @billing = Billing.new
-    @last_billing = current_user.billings.last
-    @billing.build_subscription
-  end
 
   def create
     @subscription = Subscription.find(params[:billing][:subscription_attributes][:id])
@@ -21,19 +11,5 @@ class BillingsController < ApplicationController
       @last_billing = current_user.billings.last
       render 'new'
     end
-  end
-
-  private
-
-  def set_address
-    @address = Address.find(params[:address_id])
-  end
-
-  def set_product
-    @product = Product.friendly.find(params[:product_id])
-  end
-
-  def billing_params
-    params.require(:billing).permit(:address, :first_name, :last_name, :bic, :iban, :bank, subscription_attributes: [:start_date, :id])
   end
 end
