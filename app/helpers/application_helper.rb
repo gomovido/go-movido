db6790a5e178cf0a5f3adb420762945031b5d2ec
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-05 18:41:32 +0100

    Address, countries, cities matching

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
 
-  def blue_bg_is_active?
-    (controller_name != "pages" && action_name != 'home')
-  end
-
 end
--- app/javascript/controllers/registration_controller.js
+++ app/javascript/controllers/registration_controller.js
@@ -1,16 +1,16 @@
 import { Controller } from "stimulus";
 import phoneInput from '../packs/phone-input';
-import displayAddressAutocomplete, { displayBirthCityAutocomplete } from '../packs/algolia';
+import displayAddressAutocomplete, { displayBirthCityAutocomplete, displayCityMovingAutocomplete } from '../packs/algolia';
 const places = require('places.js');
 
-
 export default class extends Controller {
-  static targets = [ "date", "phone", "dateInput", "addressInput", "checkboxAddress", "checkboxDate", "cityInput" ]
+  static targets = [ "date", "phone", "dateInput", "addressInput", "checkboxAddress", "checkboxDate", "cityInput", "cityMoving", "countryMoving" ]
 
   connect() {
     phoneInput(this.phoneTarget);
     displayAddressAutocomplete(this.addressInputTarget);
     displayBirthCityAutocomplete(this.cityInputTarget);
+    this.placeInstance = displayBirthCityAutocomplete(this.cityMovingTarget);
     this.checkboxAddressTarget.checked ? document.querySelector('.user_addresses_street').classList.add('d-none') : document.querySelector('.user_addresses_street').classList.remove('d-none');
 
   }
@@ -18,5 +18,11 @@ export default class extends Controller {
   toggleAddress() {
     document.querySelector('.user_addresses_street').classList.toggle('d-none');
     this.addressInputTarget.value = '';
+  }
+
+  updateAlgolia() {
+    this.cityMovingTarget.value = '';
+    let country = this.countryMovingTarget.value === 'France' ? 'FR' : 'GB';
+    displayCityMovingAutocomplete(this.placeInstance, country);
   }
 }
