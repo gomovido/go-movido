920d36b3a18c49061f3e87ab736cfe0160480ab6
Jordan Demmouche <jordand75020@hotmail.fr>
2020-11-05 17:03:34 +0100

    Products Index modal & filters design

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,6 @@ class Product < ApplicationRecord
   friendly_id :sku, use: :slugged
   belongs_to :category
   has_many :subscriptions, dependent: :destroy
+  has_many :product_features, dependent: :destroy
   validates :sku, presence: true
 end
