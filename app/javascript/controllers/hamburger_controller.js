8a3065b78edcc269b650ea28bf550ea18b741c66
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-09 17:09:26 +0100

    COmments updated

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -3,6 +3,14 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = [ "menuButton", "profileButton"]
 
+  connect() {
+    $('.profile-dropdown').on('hide.bs.dropdown', e => {
+    if (e.clickEvent) {
+      e.preventDefault();
+    }
+  })
+  }
+
   toggleMenu() {
     this.menuButtonTarget.classList.toggle('cross');
     $('#menuModal').modal('toggle');
