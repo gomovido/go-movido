5103630a10a6bcc20229147b30279a31cfe3ab98
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-09 19:39:13 +0100

    Validation messages

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -3,8 +3,13 @@ class Billing < ApplicationRecord
   belongs_to :user
   validates_presence_of :address, :bic, :iban, :bank
   validates_with IbanValidator
+  validate :billing_address_country
 
   def iban_prettify
     IBANTools::IBAN.new(self.iban).prettify
+  end
+
+  def billing_address_country
+     self.errors.add(:address, "needs to be in #{self.user.country}") unless self.address.split(',')[-1].strip == self.user.country
   end
 end
