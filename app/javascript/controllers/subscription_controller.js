78cf52a10534bd636a7121d7e2125c9a17708b1a
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-09 13:03:34 +0100

    Autcompleting bank inpit

--- app/javascript/controllers/subscription_controller.js
+++ app/javascript/controllers/subscription_controller.js
@@ -1,12 +1,14 @@
 import { Controller } from "stimulus";
 import { displayBillingAutocomplete } from '../packs/algolia';
+import iban_to_bank from '../packs/iban_to_bank'
 
 export default class extends Controller {
-  static targets = [ "billingAddressInput", "deliveryAddressInput", "firstPart", "secondPart" ]
+  static targets = [ "billingAddressInput", "deliveryAddressInput", "firstPart", "secondPart", "bankInput", "ibanInput" ]
 
   connect() {
     displayBillingAutocomplete(this.billingAddressInputTarget);
     displayBillingAutocomplete(this.deliveryAddressInputTarget);
+    this.bankInputTarget.value = iban_to_bank(this.ibanInputTarget.value);
   }
 
   stepForward() {
@@ -21,5 +23,9 @@ export default class extends Controller {
     this.firstPartTarget.classList.remove('d-none');
     this.secondPartTarget.classList.add('d-none');
     document.querySelector('.step-title').innerText = 'Please complete your payment details';
+  }
+
+  fillBank() {
+    this.bankInputTarget.value = iban_to_bank(this.ibanInputTarget.value);
   }
 }
