0348b83601494f0fa17e8ba0b54cd872148bb2a0
Jordan Demmouche <jordand75020@hotmail.fr>
2020-11-19 14:13:30 +0100

    Redirect to congratulation if subscription is already paid

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -10,16 +10,16 @@ class ChargesController < ApplicationController
 
   def process_payment(subscription, stripe_token)
     begin
-    stripe_charge = Stripe::Charge.create({
-        amount: subscription.product.sim_card_price_cents,
-        currency: subscription.product.sim_card_price_currency,
-        source: stripe_token,
-        description: "Subscription ##{subscription.id}",
-      })
-    subscription.update(state: 'paid')
-    create_charge(stripe_charge, subscription)
-    UserMailer.with(user: subscription.address.user, subscription: subscription).congratulations.deliver_now
-    redirect_to subscription_congratulations_path(subscription)
+      stripe_charge = Stripe::Charge.create({
+          amount: subscription.product.sim_card_price_cents,
+          currency: subscription.product.sim_card_price_currency,
+          source: stripe_token,
+          description: "Subscription ##{subscription.id}",
+        })
+      charge = create_charge(stripe_charge, subscription)
+      subscription.update(state: 'paid') if charge.status == 'succeeded'
+      UserMailer.with(user: subscription.address.user, subscription: subscription).congratulations.deliver_now
+      redirect_to subscription_congratulations_path(subscription)
     rescue Stripe::CardError => e
       flash[:alert] = e
       redirect_back(fallback_location: root_path)
@@ -28,11 +28,12 @@ class ChargesController < ApplicationController
 
 
   def create_charge(stripe_charge, subscription)
-    Charge.create(
+    charge = Charge.create(
       stripe_charge_id: stripe_charge.id,
       status: stripe_charge.status,
       subscription_id: subscription.id,
     )
+    return charge
   end
 
   private
