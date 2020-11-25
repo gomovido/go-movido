ab1d0e58813db3a7369f924c5c85f08f1b6ea506
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-25 13:34:15 +0100

    Legals + refacto controllers

---
+++ app/javascript/controllers/privacy_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+
+  connect() {
+    this.privacy(document, 'script', 'termly-jssdk');
+  }
+  privacy(d, s, id) {
+    var js, tjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://app.termly.io/embed-policy.min.js";
+    tjs.parentNode.insertBefore(js, tjs);
+  };
+}
