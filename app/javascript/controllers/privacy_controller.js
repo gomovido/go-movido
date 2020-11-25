43a9856912977815bdda85fe5082b1ba943a80cf
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-25 14:11:08 +0100

    Cookies

--- app/javascript/controllers/privacy_controller.js
+++ app/javascript/controllers/privacy_controller.js
@@ -7,7 +7,6 @@ export default class extends Controller {
   }
   privacy(d, s, id) {
     var js, tjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
     js = d.createElement(s);
     js.id = id;
     js.src = "https://app.termly.io/embed-policy.min.js";
