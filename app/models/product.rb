091591d004561642c33a5aa3dad84b3302720c3d
Jordan Demmouche <jordand75020@hotmail.fr>
2020-11-25 11:45:16 +0100

    Manage Product set_sim_card_price_currency

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,19 @@ class Product < ApplicationRecord
   has_many :product_features, dependent: :destroy
   validates :sku, presence: true
   monetize :sim_card_price_cents
+  after_create :set_sim_card_price_currency
 
 
   def eligible?(user)
     self.category.name == 'mobile' ? true : user.active_address.valid_address
+  end
+
+
+  def set_sim_card_price_currency
+    if self.country == 'United Kingdom'
+      self.update(sim_card_price_currency: 'GBP')
+    else
+      self.update(sim_card_price_currency: 'EUR')
+    end
   end
 end
