5874d42e36582d57664e536dbb5f539f2639edf8
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-26 13:09:54 +0100

    Banner into application

---
+++ app/javascript/packs/cookie_banner.js
@@ -0,0 +1,10 @@
+export default function cookie() {
+  const s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = 'https://app.termly.io/embed.min.js';
+  s.id = '780e5677-086c-403b-bffc-0418efa73946';
+  s.setAttribute("data-name", "termly-embed-banner");
+  const x = document.getElementsByTagName('script')[0];
+  x.parentNode.insertBefore(s, x);
+};
