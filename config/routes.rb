6837bdfcc5f2d8dbcec94201916c518ca7fdcd83
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-26 10:45:46 +0100

    Legals

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   get 'privacy', to: 'pages#privacy'
   get 'cookies', to: 'pages#cookies'
+  get 'terms', to: 'pages#terms'
   get 'summary/:subscription_id', to: 'subscriptions#summary', as: 'subscription_summary'
   get 'payment/:subscription_id', to: 'subscriptions#payment', as: 'subscription_payment'
   patch 'validate_subscription/:subscription_id', to: 'subscriptions#validate_subscription', as: 'validate_subscription'
