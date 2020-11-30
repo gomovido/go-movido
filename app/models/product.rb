5fc4a19a021e3a158c06a8e16666d6ec2ccce73c
Yoni Touboul <yonitouboul@hotmail.com>
2020-11-30 16:52:24 +0100

    Fixed validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,14 +7,7 @@ class Product < ApplicationRecord
   monetize :sim_card_price_cents
   after_create :set_sim_card_price_currency
 
-  #presence
-  validates_presence_of :company, :name, :description, :price, :logo_url, :call_limit, :data_limit, :time_contract, :unlimited_call, :unlimited_data, :obligation
-  # conditional & special formats
-  validates :price, numericality: { greater_than_or_equal_to: 1,  only_integer: true }, if: :sim_needed?
-  validates :logo_url, url: true
-  validates :call_limit, inclusion: { in: %w[unlimited Unlimited] }, if: :unlimited_call?
-  validates :data_limit, inclusion: { in: %w[unlimited Unlimited] }, if: :unlimited_data?
-  validates :time_contract, inclusion: { in: %w[no No] }, unless: :obligation?
+
 
   def is_wifi?
     self.category.name == 'wifi'
