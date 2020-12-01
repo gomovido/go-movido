1b901c84a7b2869cfcacf840469855cc3454ac6c
jordan demmouche <jordand75020@hotmail.fr>
2020-12-02 00:51:35 +0100

    Navbar visitor with scrolling effect

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = [ "navbar"]
+
+  onScroll() {
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+      this.navbarTarget.classList.add('fixed-nav')
+    } else if (document.body.scrollTop < 1 || document.documentElement.scrollTop < 1) {
+      this.navbarTarget.classList.remove('fixed-nav')
+    }
+  }
+}
