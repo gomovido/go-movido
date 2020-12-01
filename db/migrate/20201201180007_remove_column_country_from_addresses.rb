11e55a2929d48e4e671b994dea1c45349703dc24
jordan demmouche <jordand75020@hotmail.fr>
2020-12-01 19:49:13 +0100

    db:rollback on country in addresses table

--- db/migrate/20201201180007_remove_column_country_from_addresses.rb
+++ db/migrate/20201201180007_remove_column_country_from_addresses.rb
@@ -1,5 +1,5 @@
 class RemoveColumnCountryFromAddresses < ActiveRecord::Migration[6.0]
   def change
-    remove_column :addresses, :country
+    remove_column :addresses, :country, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_180007) do
+ActiveRecord::Schema.define(version: 2020_12_01_143832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_180007) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active"
     t.boolean "valid_address"
+    t.string "country"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
