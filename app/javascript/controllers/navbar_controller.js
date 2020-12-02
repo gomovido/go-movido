f1b2b5e7b94ee786fa634107c3d4c3e92ebcfc88
Yoni Touboul <yonitouboul@hotmail.com>
2020-12-02 16:50:10 +0100

    Buttons facebook & google

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -5,12 +5,9 @@ export default class extends Controller {
 
   onScroll() {
     if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-      if(document.body.scrollHeight > document.body.clientHeight){
-         this.navbarTarget.classList.add('fixed-nav')
-}
-
+      this.navbarTarget.classList.add('fixed-nav');
     } else if (document.body.scrollTop < 1 || document.documentElement.scrollTop < 1) {
-      this.navbarTarget.classList.remove('fixed-nav')
+      this.navbarTarget.classList.remove('fixed-nav');
     }
   }
 }
