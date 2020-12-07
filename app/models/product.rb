3934d14f2b6fb4c9f5f41acff4c6f6e5aed7f9c4
Yoni Touboul <yonitouboul@hotmail.com>
2020-12-07 12:38:06 +0100

    remove .currency method

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,6 @@ class Product < ApplicationRecord
   monetize :sim_card_price_cents
   after_create :set_sim_card_price_currency
 
-
-  def currency
-    self.sim_card_price_currency == 'EUR' ? '€' : '£'
-  end
-
   def is_wifi?
     self.category.name == 'wifi'
   end
