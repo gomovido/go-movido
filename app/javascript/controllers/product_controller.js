3e61ad27394e2c3fa29e4a53952d4ba85aea5cbd
Yoni Touboul <yonitouboul@hotmail.com>
2020-12-08 04:15:31 +0100

    Product cards, steps, positioning, bubbles & modal

--- app/javascript/controllers/product_controller.js
+++ app/javascript/controllers/product_controller.js
@@ -1,10 +1,14 @@
 import ApplicationController from './application_controller'
 
 export default class extends ApplicationController {
-  static targets = ['modalBody']
+  static targets = ['modalBody', 'link']
 
   loadProduct(event) {
     $(".modal-body").load(event.target.getAttribute("data-href"));
   }
-
+  redirect() {
+    if (event.target.dataset.target !== '#exampleModal') {
+      this.linkTargets[0].click();
+    }
+  }
 }
