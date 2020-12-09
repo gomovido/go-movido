9befeb24a2440181512533643fabeeb26350872d
jordan demmouche <jordand75020@hotmail.fr>
2020-12-09 18:27:27 +0100

    Padding button-exit & disable continue button

--- app/javascript/controllers/subscription_controller.js
+++ app/javascript/controllers/subscription_controller.js
@@ -17,14 +17,12 @@ export default class extends Controller {
     window.scroll({top: 0});
     this.firstPartTarget.classList.add('d-none');
     this.secondPartTarget.classList.remove('d-none');
-    document.querySelector('.step-title').innerText = 'Please choose your SIM Card format';
   }
 
   stepBackward() {
     window.scroll({top: 0});
     this.firstPartTarget.classList.remove('d-none');
     this.secondPartTarget.classList.add('d-none');
-    document.querySelector('.step-title').innerText = 'We just need a couple of more details';
   }
 
   fillBank() {
