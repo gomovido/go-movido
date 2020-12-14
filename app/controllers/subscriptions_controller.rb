2c6ac007ed9f8a4fb3d17cc6764dd46a37d1ff02
Yoni Touboul <yonitouboul@hotmail.com>
2020-12-14 18:19:50 +0100

    Flashses translations, payment & testimonaisl

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -55,7 +55,7 @@ class SubscriptionsController < ApplicationController
   def active_address_do_not_exist?(product)
     if current_user.active_address.nil? || (!current_user.active_address.valid_address && !product.is_mobile?)
       redirect_to user_path(current_user)
-      flash[:alert] = "You have to create / select an address in #{product.country}"
+      flash[:alert] = I18n.t 'flashes.wrong_country', country: product.country
     end
   end
 
@@ -63,7 +63,7 @@ class SubscriptionsController < ApplicationController
     subscription_check = Subscription.find_by(state: 'succeeded', product: product, address: current_user.active_address)
     if subscription_check && product.category.name != 'mobile'
       redirect_to subscription_congratulations_path(subscription_check)
-      flash[:alert] = 'You already have this subscription !'
+      flash[:alert] = I18n.t 'flashes.existing_subscription'
     end
   end
 
