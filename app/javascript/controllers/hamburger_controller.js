c250dfd279b626367d6e09c6165050e9ba05219f
Yoni Touboul <yonitouboul@hotmail.com>
2020-12-23 12:37:02 +0100

    Updated classes typo

--- app/javascript/controllers/hamburger_controller.js
+++ app/javascript/controllers/hamburger_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "stimulus";
 
 export default class extends Controller {
   static targets = [ "menuButton"]
+  static classes = [ "cross" ]
 
   toggleMenu() {
-    this.menuButtonTarget.classList.toggle('cross');
+    this.menuButtonTarget.classList.toggle(this.crossClass);
     $('#menuModal').modal('toggle');
   }
 }
