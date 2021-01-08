4cc6491abcb2a5ab00a39a7e1869a47be343aefe
Yoni Touboul <yonitouboul@hotmail.com>
2021-01-08 13:05:22 +0100

    Profile new

--- app/javascript/controllers/profil_form_controller.js
+++ app/javascript/controllers/profil_form_controller.js
@@ -1,15 +1,33 @@
-import ApplicationController from './application_controller'
+import ApplicationController from './application_controller.js'
 import phoneInput from '../packs/phone-input';
+import StimulusReflex from 'stimulus_reflex';
 import {addressAutocomplete, searchByCity, searchByCountry, autoFill} from '../packs/algolia';
 const places = require('places.js');
 
 export default class extends ApplicationController {
-  static targets = ['phone', 'input', 'addressInput', 'addAddressInput', 'addAddressButton', 'birthCity'];
+  static targets = ['phone', 'input', 'addressInput', 'addAddressInput', 'addAddressButton', 'birthCity', 'errors'];
   static classes = [ 'hide', 'flex', 'readonly' ]
+
+  connect() {
+    StimulusReflex.register(this);
+    this.birthCity = addressAutocomplete(this.birthCityTarget);
+    searchByCity(this.birthCity);
+    phoneInput(this.phoneTarget);
+    this.phoneTarget.value = document.getElementById('user_phone').value;
+  }
 
   updateNavbar(event) {
     if (event.target.value) {
-      document.getElementById('navbarDropdown').innerText = document.getElementById('user_first_name').value + ' ' + document.getElementById('user_last_name').value;
+      document.getElementById('profileNavbar').innerText = document.getElementById('user_first_name').value + ' ' + document.getElementById('user_last_name').value;
+    }
+  }
+
+  afterReflex(event, response, error) {
+    if (error) {
+      this.errorsTarget.innerHTML = "";
+      this.errorsTarget.insertAdjacentHTML('beforeend', error);
+    } else {
+      this.connect();
     }
   }
 
@@ -22,28 +40,5 @@ export default class extends ApplicationController {
     this.addAddressInputTarget.classList.add(this.flexClass);
     this.addAddressButtonTarget.classList.add(this.hideClass);
     this.addAddressButtonTarget.classList.remove(this.flexClass);
-  }
-
-  toggleInput(event) {
-    event.target.style.setProperty('display', 'none', 'important')
-    let inputs = this.inputTargets
-    inputs.push(this.phoneTarget);
-    inputs.push(this.birthCityTarget);
-    inputs.forEach(input => {
-      if (input.dataset.id === event.target.dataset.id) {
-        input.removeAttribute('readonly');
-        input.classList.remove(this.readonlyClass);
-        input.parentNode.classList.remove(this.readonlyClass);
-         input.parentNode.parentNode.classList.remove(this.readonlyClass);
-        if (input === this.phoneTarget) {
-          phoneInput(this.phoneTarget);
-          this.phoneTarget.value = document.getElementById('user_phone').value;
-        } if (input === this.birthCityTarget) {
-          this.birthCity = addressAutocomplete(this.birthCityTarget);
-          searchByCity(this.birthCity);
-        }
-      }
-    });
-    event.target.nextElementSibling.classList.remove(this.hideClass);
   }
 }
