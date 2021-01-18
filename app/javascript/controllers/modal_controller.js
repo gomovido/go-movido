65692db9e37984ee536ef3287384566d775d7d27
jordan demmouche <jordand75020@hotmail.fr>
2021-01-18 15:37:38 +0100

    Products cards & modal suite

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,14 +1,9 @@
 import ApplicationController from './application_controller'
 
 export default class extends ApplicationController {
-  static targets = ['modalBody', 'link']
+  static targets = ['modalBody']
 
   loadModal(event) {
     $(".modal-body").load(event.target.getAttribute("data-href"));
-  }
-  redirect(event) {
-    if (event.target.dataset.target !== '#exampleModal') {
-      this.linkTargets[0].click();
-    }
   }
 }
