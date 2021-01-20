b81188e4fda8402005b09eddebb11750af3a6a52
jordan demmouche <jordand75020@hotmail.fr>
2021-01-20 19:01:15 +0100

    Stripe Error handling & stripe api key production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,10 @@
 Rails.configuration.stripe = {
-  publishable_key: Rails.application.credentials.development[:stripe][:publishable_key],
-  secret_key:  Rails.application.credentials.development[:stripe][:secret_key]
+  if Rails.env.development?
+    publishable_key: Rails.application.credentials.development[:stripe][:publishable_key],
+    secret_key:  Rails.application.credentials.development[:stripe][:secret_key]
+  else
+    publishable_key: Rails.application.credentials.production[:stripe][:publishable_key],
+    secret_key:  Rails.application.credentials.production[:stripe][:secret_key]
+  end
 }
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
