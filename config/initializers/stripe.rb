b5c390ace0387124103170098f8b6b73e3192c3a
jordan demmouche <jordand75020@hotmail.fr>
2021-01-20 19:03:06 +0100

    Check environement in stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,10 +1,12 @@
-Rails.configuration.stripe = {
-  if Rails.env.development?
+if Rails.env.development?
+  Rails.configuration.stripe = {
     publishable_key: Rails.application.credentials.development[:stripe][:publishable_key],
     secret_key:  Rails.application.credentials.development[:stripe][:secret_key]
-  else
+  }
+else
+  Rails.configuration.stripe = {
     publishable_key: Rails.application.credentials.production[:stripe][:publishable_key],
     secret_key:  Rails.application.credentials.production[:stripe][:secret_key]
-  end
-}
+  }
+end
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
