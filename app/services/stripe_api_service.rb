d2dd895f5605da9c51b1ef9b7d503f5e79ea00bb
jordan demmouche <jordand75020@hotmail.fr>
2021-01-21 12:39:53 +0100

    Create customer on stripe on proceed payment

--- app/services/stripe_api_service.rb
+++ app/services/stripe_api_service.rb
@@ -2,23 +2,55 @@ class StripeApiService
   def initialize(params)
     @stripe_token = params[:stripe_token]
     @subscription_id = params[:subscription_id]
+    @user_id = params[:user_id]
   end
 
-  def create_charge
+
+  def proceed_stripe
+    response = manage_stripe_customer
+    response[:customer] ? create_charge(response[:customer].id) : response
+  end
+
+  def create_charge(customer_id)
     subscription = Subscription.find(@subscription_id)
     begin
       stripe_charge = Stripe::Charge.create({
           amount: subscription.product.sim_card_price_cents,
           currency: subscription.product.currency,
-          source: @stripe_token,
+          customer: customer_id,
           description: "Subscription ##{subscription.id}",
         })
-      hash = {stripe_charge: stripe_charge, error: nil}
-      return hash
+      return {stripe_charge: stripe_charge, error: nil}
     rescue Stripe::CardError => e
-      hash = {stripe_charge: stripe_charge, error: e}
+      return {stripe_charge: stripe_charge, error: e}
+    end
+  end
+
+
+  def manage_stripe_customer
+    user = User.find(@user_id)
+    user.stripe_id.nil? ? create_customer(user) : update_customer(user)
+  end
+
+  def create_customer(user)
+    begin
+      customer = Stripe::Customer.create( email: user.email, source: @stripe_token, description: "Customer ##{user.id} - #{user.email}")
+      user.update(stripe_id: customer.id)
+      return {customer: customer, error: nil}
+    rescue Stripe::InvalidRequestError => error
+      hash = {customer: customer, error: error}
       return hash
     end
   end
 
+  def update_customer(user)
+    begin
+      customer = Stripe::Customer.update( "#{user.stripe_id}", source: @stripe_token)
+      return {customer: customer, error: nil}
+    rescue Stripe::CardError => error
+      return {customer: customer, error: error}
+    rescue Stripe::InvalidRequestError => error
+      return {customer: customer, error: error}
+    end
+  end
 end
