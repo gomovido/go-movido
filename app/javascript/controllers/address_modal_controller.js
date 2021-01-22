5f662279757f54bd5d05bbb648ddaca2cdcafa29
Yoni Touboul <yonitouboul@hotmail.com>
2021-01-22 18:45:13 +0100

    Modal addresses

---
+++ app/javascript/controllers/address_modal_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "stimulus";
+import {addressAutocomplete, autoFill, searchByCountry} from '../packs/algolia';
+import StimulusReflex from 'stimulus_reflex';
+const places = require('places.js');
+
+export default class extends Controller {
+  static targets = ["streetInput", "button"]
+  static values = {country: String}
+
+  connect() {
+    StimulusReflex.register(this);
+    const addressInput = addressAutocomplete(this.streetInputTarget);
+    searchByCountry(addressInput, [this.countryValue])
+    addressInput.on('change', (e) => {
+      document.querySelector('#address_zipcode').value = e.suggestion.postcode
+      document.querySelector('#address_city').value = e.suggestion.county
+      this.streetInputTarget.classList.remove('is-invalid')
+      this.streetInputTarget.classList.add('is-valid')
+      this.stimulate('AddressReflex#create_with_modal', this.streetInputTarget)
+        .then(() => {
+          this.buttonTarget.classList.remove('disabled');
+        });
+    });
+  }
+
+
+  borderColor() {
+    if (document.querySelector('#address_zipcode').value === "" || document.querySelector('#address_city').value === "" || this.streetInputTarget.value === "") {
+      this.streetInputTarget.classList.remove('is-valid');
+      this.streetInputTarget.classList.add('is-invalid');
+    }
+  }
+}
--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,7 @@ Rails.application.routes.draw do
   get 'products/modal/:id', to: 'products#modal', as: 'modal_product'
   get 'subscriptions/modal/:id', to: 'subscriptions#modal', as: 'modal_subscription'
 
+
   resources :categories, only: [:index] do
     resources :banks, only: [:index]
     resources :products, only: [:index] do
