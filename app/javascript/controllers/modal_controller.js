6db231eabcd49fc5e5679735f5f8438218365653
Yoni Touboul <yonitouboul@hotmail.com>
2021-01-25 00:53:38 +0100

    Fixed modals toggle

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -6,6 +6,10 @@ export default class extends ApplicationController {
     $(".product-body").load(event.target.getAttribute("data-href"));
   }
 
+  loadSubscriptionModal(event) {
+    $('.modal-body').load(event.target.getAttribute("data-href"));
+  }
+
   updateButton(event) {
     let id = event.target.dataset.product;
     document.querySelector('#addressButton').href=`/categories/wifi/products/${id}/subscriptions`
