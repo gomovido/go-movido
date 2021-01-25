cae212fb6be67c04c7223c34186ebca8bc7f14f4
jordan demmouche <jordand75020@hotmail.fr>
2021-01-25 18:04:44 +0100

    Localize emails & subscriptions

--- app/controllers/users/_omniauth_callbacks_controller.rb
+++ app/controllers/users/_omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
-    @user = User.from_omniauth_google(request.env['omniauth.auth'])
+    @user = User.from_omniauth_google(request.env)
     I18n.locale = request.env["omniauth.params"]["locale"]
     if @user.persisted?
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
@@ -12,17 +12,31 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    I18n.locale = request.env["omniauth.params"]["locale"]
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook' if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    end
-  end
+  # def facebook
+  #   @user = from_omniauth_facebook(request.env["omniauth.auth"])
+  #   I18n.locale = request.env["omniauth.params"]["locale"]
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user, :event => :authentication
+  #     flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook' if is_navigational_format?
+  #   else
+  #     session["devise.facebook_data"] = request.env["omniauth.auth"]
+  #     redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+  #   end
+  # end
+
+  # def from_omniauth_facebook(auth)
+  #   user = User.find_by(provider: auth.provider, uid: auth.uid)
+  #   unless user
+  #     user = User.create(
+  #       email: auth.info.email,
+  #       password: Devise.friendly_token[0,20],
+  #       first_name: auth.info.name.split(' ')[0],
+  #       last_name: auth.info.name.split(' ').drop(1).join('-')
+  #     )
+  #     UserMailer.with(user: user, locale: locale).welcome_email.deliver_now
+  #   end
+  #   return user
+  # end
 
 
   def failure
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,35 +2,35 @@ class UserMailer < ApplicationMailer
 
   def welcome_email
     @user = params[:user]
-    @locale = I18n.locale
+    @locale = params[:locale]
     mail(to: @user.email, subject: I18n.t('mail.welcome_email'))
   end
 
   def subscription_under_review_email
     @user = params[:user]
     @subscription = params[:subscription]
-    @locale = I18n.locale
-    mail(to: @user.email, subject: I18n.t('mail.subscription_under_review'))
-  end
-
-  def booking_under_review_email
-    @user = params[:user]
-    @booking = params[:booking]
-    @locale = I18n.locale
+    @locale = params[:locale]
     mail(to: @user.email, subject: I18n.t('mail.subscription_under_review'))
   end
 
   def subscription_confirmed_email
     @user = params[:user]
     @subscription = params[:subscription]
-    @locale = I18n.locale
+    @locale = params[:locale]
     mail(to: @user.email, subject:  I18n.t('mail.subscription_confirmed'))
+  end
+
+  def booking_under_review_email
+    @user = params[:user]
+    @booking = params[:booking]
+    @locale = params[:locale]
+    mail(to: @user.email, subject: I18n.t('mail.subscription_under_review'))
   end
 
   def booking_confirmed_email
     @user = params[:user]
     @booking = params[:booking]
-    @locale = I18n.locale
+    @locale = params[:locale]
     mail(to: @user.email, subject: I18n.t('mail.booking_confirmed'))
   end
 
