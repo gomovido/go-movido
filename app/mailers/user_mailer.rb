edc15f07f20be3cc6a6996dfd98cb0e5c4bc4d69
jordan demmouche <jordand75020@hotmail.fr>
2021-01-25 15:01:49 +0100

    Set local in every mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,28 +9,28 @@ class UserMailer < ApplicationMailer
   def subscription_under_review_email
     @user = params[:user]
     @subscription = params[:subscription]
-    @locale = params[:locale]
+    @locale = I18n.locale
     mail(to: @user.email, subject: I18n.t('mail.subscription_under_review'))
   end
 
   def booking_under_review_email
     @user = params[:user]
     @booking = params[:booking]
-    @locale = params[:locale]
+    @locale = I18n.locale
     mail(to: @user.email, subject: I18n.t('mail.subscription_under_review'))
   end
 
   def subscription_confirmed_email
     @user = params[:user]
     @subscription = params[:subscription]
-    @locale = params[:locale]
+    @locale = I18n.locale
     mail(to: @user.email, subject:  I18n.t('mail.subscription_confirmed'))
   end
 
   def booking_confirmed_email
     @user = params[:user]
     @booking = params[:booking]
-    @locale = params[:locale]
+    @locale = I18n.locale
     mail(to: @user.email, subject: I18n.t('mail.booking_confirmed'))
   end
 
