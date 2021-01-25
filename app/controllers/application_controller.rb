300c002dac647903e74a4967cb71fb8e6e5b97a5
jordan demmouche <jordand75020@hotmail.fr>
2021-01-25 12:43:52 +0100

    Clean localize code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,7 +32,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
       keys: [
-        :email, :first_name, :last_name, :phone, :birthdate, :city, :locale,
+        :email, :first_name, :last_name, :phone, :birthdate, :city,
         :country, :not_housed, :birth_city, addresses_attributes: [:street, :zipcode, :country, :city]
       ]
     )
