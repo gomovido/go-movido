883d651020ef771c3236a3a7a2b1e932443d4cd6
Yoni Touboul <yonitouboul@hotmail.com>
2021-01-29 18:51:40 +0100

    Put date input back

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,14 +1,4 @@
 import Flatpickr from "stimulus-flatpickr";
 import "flatpickr/dist/themes/dark.css";
 
-export default class extends Flatpickr {
-  static targets = ['flatpickr']
-
-  connect() {
-    if (document.querySelector('.flatpickr-mobile')) {
-      document.querySelector('.flatpickr-mobile').addEventListener('change', e => {
-        document.querySelector('.flatpickr-mobile').placeholder = '';
-      });
-    }
-  }
-}
+export default class extends Flatpickr {}
