f39ddbb12510dc7fbd10c39025f15fbadebf10de
Yoni Touboul <yonitouboul@hotmail.com>
2021-01-29 18:11:13 +0100

    Safari Ios Data input + logout

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,4 +1,14 @@
 import Flatpickr from "stimulus-flatpickr";
 import "flatpickr/dist/themes/dark.css";
 
-export default class extends Flatpickr {}
+export default class extends Flatpickr {
+  static targets = ['flatpickr']
+
+  connect() {
+    if (document.querySelector('.flatpickr-mobile')) {
+      document.querySelector('.flatpickr-mobile').addEventListener('change', e => {
+        document.querySelector('.flatpickr-mobile').placeholder = '';
+      });
+    }
+  }
+}
