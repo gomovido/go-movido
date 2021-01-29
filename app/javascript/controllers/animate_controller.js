0f2eb8fe3444f842bbce41194b6fe76a88669def
jordan demmouche <jordand75020@hotmail.fr>
2021-01-29 04:56:44 +0100

    How it works

--- app/javascript/controllers/animate_controller.js
+++ app/javascript/controllers/animate_controller.js
@@ -8,7 +8,13 @@ export default class extends ApplicationController {
     let service = event.currentTarget.dataset.serviceName
     document.getElementById(service).classList.toggle('open-card')
     document.getElementById(service).getElementsByClassName("card-link")[0].classList.toggle('d-none')
+    let icon = document.getElementById(service).getElementsByClassName("far")[0]
+    if (icon) {
+      icon.classList.toggle('fa-plus')
+      icon.classList.toggle('fa-window-minimize')
+    }
   }
+
 
   slideRight() {
     this.containerTarget.scrollLeft += 2000;
