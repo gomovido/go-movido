9a5186df982c3c8d6ffed2171f16385bed4f0d78
Yoni Touboul <yonitouboul@hotmail.com>
2021-01-29 19:12:46 +0100

    Added super connect

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,6 +3,7 @@ import "flatpickr/dist/themes/dark.css";
 
 export default class extends Flatpickr {
   connect() {
+    super.connect();
     let field = document.querySelector('.flatpickr-mobile');
     if (field) {
       field.addEventListener('change', e => {
