643aaa9b1696f12c489d6ef19aba9bc97f9b6ccb
jordan demmouche <jordand75020@hotmail.fr>
2021-02-02 15:12:46 +0100

    Modal Bank

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,9 +1,14 @@
 class BanksController < ApplicationController
 
-
   def index
     @banks = Bank.all
   end
 
+  def modal
+    @bank = Bank.find(params[:id])
+    respond_to do |format|
+      format.html { render layout: false }
+    end
+  end
 
 end
