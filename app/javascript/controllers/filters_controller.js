7944a091fe6ff8e365bab909d91c77f57261c4e3
Yoni Touboul <yonitouboul@hotmail.com>
2021-02-08 17:33:04 +0100

    Test travis

--- app/javascript/controllers/filters_controller.js
+++ app/javascript/controllers/filters_controller.js
@@ -7,6 +7,7 @@ export default class extends ApplicationController {
   static targets = ['input', 'noResult']
 
   connect() {
+    console.log('done')
     $(window).bind("pageshow", function(event) {
       if (event.originalEvent.persisted) {
         window.location.reload()
