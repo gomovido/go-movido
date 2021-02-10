fb3dbe2788d0d5a9770a401e1880c3ab81b61c4d
jordan demmouche <jordand75020@hotmail.fr>
2021-02-10 15:31:03 +0100

    test should not pass on travis

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  context 'User should have first_name, last_name & email' do
+  context 'User should be created' do
     it "is not valid without a first_name" do
-      user = User.new(first_name: nil)
-      expect(user).to_not be_valid
+      user = User.new(first_name: "test", last_name: "test", email: 'test@test.com')
+      expect(user).to be_valid
     end
   end
 end
