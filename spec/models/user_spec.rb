af6b84e965a284647c4c749eba7c45c98b75df70
jordan demmouche <jordand75020@hotmail.fr>
2021-02-10 15:50:05 +0100

    test should passa

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe User do
   context 'User should be created' do
     it "is not valid without a first_name" do
-      user = User.new(first_name: "test", last_name: "test", email: 'test@test.com')
-      expect(user).to be_valid
+      user = User.new(first_name: nil)
+      expect(user).to_not be_valid
     end
   end
 end
