dca6f3adda8f298c68528f323a765b3405a2ae3f
jordan demmouche <jordand75020@hotmail.fr>
2021-02-10 16:52:54 +0100

    Add factoryBot

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User do
   context 'User should be created' do
-    it "is not valid without a first_name" do
-      user = User.new(first_name: nil)
-      expect(user).to_not be_valid
+    it "is valid with full columns" do
+      user = build(:user)
+      expect(user).to be_valid
     end
   end
 end
