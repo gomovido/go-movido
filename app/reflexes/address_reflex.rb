374a3e8cc907699a3f56d555f1cbb8413a560129
jordan demmouche <jordand75020@hotmail.fr>
2021-02-11 17:02:49 +0100

    addresses without submit reflex

--- app/reflexes/address_reflex.rb
+++ app/reflexes/address_reflex.rb
@@ -5,8 +5,8 @@ class AddressReflex < ApplicationReflex
     address = Address.find(element.dataset.id)
     address.set_has_active
     current_user.update_user_country
-    morph ".subscriptions-wrapper", with_locale {render(partial: "subscriptions", locals: {subscriptions: current_user.subscriptions})}
-    morph ".addresses-container",  with_locale {render(partial: "addresses", locals: {address: Address.new, active_address: address, addresses: current_user.addresses.where(active: false)})}
+    morph ".subscriptions-wrapper", with_locale {render(partial: "users/desktop/subscriptions", locals: {subscriptions: current_user.subscriptions})}
+    morph ".addresses-container",  with_locale {render(partial: "users/desktop/addresses", locals: {address: Address.new, active_address: address, addresses: current_user.addresses.where(active: false)})}
   end
 
   def create
@@ -17,8 +17,8 @@ class AddressReflex < ApplicationReflex
     if @address.save
       @address.set_has_active
       current_user.update_user_country
-      morph ".subscriptions-wrapper", with_locale {render(partial: "subscriptions", locals: {subscriptions: current_user.subscriptions})}
-      morph ".addresses-container",  with_locale {render(partial: "addresses", locals: {address: Address.new, active_address: @address, addresses: current_user.addresses.where(active: false)})}
+      morph ".subscriptions-wrapper", with_locale {render(partial: "users/desktop/subscriptions", locals: {subscriptions: current_user.subscriptions})}
+      morph ".addresses-container",  with_locale {render(partial: "users/desktop/addresses", locals: {address: Address.new, active_address: @address, addresses: current_user.addresses.where(active: false)})}
     else
       morph '#error', with_locale {I18n.t('users.addresses.form.failure.street')}
     end
