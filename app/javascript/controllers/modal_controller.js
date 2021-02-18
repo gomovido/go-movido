b809b0146cf6a45f3e8b7ecab4fafbdcd3577063
yonitou <61981305+yonitou@users.noreply.github.com>
2021-02-18 18:06:37 +0100

    Update modal_controller.js

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -12,7 +12,6 @@ export default class extends ApplicationController {
   }
 
   loadSubscriptionModal(event) {
-    console.log(event.target)
     $('.modal-body').load(event.target.getAttribute("data-href"));
   }
 
