fe9ba023041e982c6d9daa949812e99bef7c8952
Yoni Touboul <yonitouboul@hotmail.com>
2021-02-18 19:58:20 +0100

    Deleted last migrations

--- db/migrate/20210218150640_remove_company_columns_from_products.rb
+++
@@ -1,6 +0,0 @@
-class RemoveCompanyColumnsFromProducts < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :products, :company_name, :string
-    remove_column :products, :logo_url, :string
-  end
-end
