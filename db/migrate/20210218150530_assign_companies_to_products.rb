5fc12781aa2b2c0ea3af80d9f79f5c94874ee5fe
Yoni Touboul <yonitouboul@hotmail.com>
2021-02-18 19:58:20 +0100

    Deleted last migrations

--- db/migrate/20210218150530_assign_companies_to_products.rb
+++
@@ -1,5 +0,0 @@
-class AssignCompaniesToProducts < ActiveRecord::Migration[6.0]
-  def change
-    Product.all.each  {|product| product.update(company: Company.find_by(name: product.company_name)) }
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_18_150640) do
+ActiveRecord::Schema.define(version: 2021_02_18_145311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,8 @@ ActiveRecord::Schema.define(version: 2021_02_18_150640) do
     t.float "setup_price"
     t.string "currency"
     t.boolean "active"
+    t.string "logo_url"
+    t.string "company_name"
     t.bigint "company_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["company_id"], name: "index_products_on_company_id"
