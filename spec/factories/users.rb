294e682d8cc56bcf775d7f10c453c18b5548cd2e
jordan demmouche <jordand75020@hotmail.fr>
2021-02-25 17:45:23 +0100

    Change password in factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name  { Faker::Name.last_name  }
     email  { Faker::Internet.email }
-    password { "123456" }
+    password { "1234567" }
   end
 end
