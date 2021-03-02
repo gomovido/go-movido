a183abb46589cd25e99da0f332db1e29ddac09d5
jordan demmouche <jordand75020@hotmail.fr>
2021-03-02 13:20:51 +0100

    Change spec feature

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     birthdate { Faker::Date.birthday(min_age: 18, max_age: 65) }
     birth_city  { Faker::Nation.capital_city }
     trait :fr do
-      phone  { "+33767669502" }
+      phone  { "+33767669503" }
     end
     trait :gb do
       phone  { "+447700900676" }
