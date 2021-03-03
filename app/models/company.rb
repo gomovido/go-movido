c8ce92c12687cf59ae6552e504778ddffc866e06
Yoni Touboul <yonitouboul@hotmail.com>
2021-03-03 19:18:01 +0100

    Removed validations format of

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,8 +6,6 @@ class Company < ApplicationRecord
 
   validates_presence_of :name, :logo_url, :policy_link
   validates_uniqueness_of :name
-  validates :policy_link, format: { with: URI.regexp }
   validates :logo_url, format: {with: /\.(png|jpg|svg)\Z/i}
-  validates :cancel_link, format: { with: URI.regexp }, if: :cancel_link?
 
 end
