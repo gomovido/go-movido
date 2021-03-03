b901d45b5f0add660d20d9d9c34ff1266da84a8a
Yoni Touboul <yonitouboul@hotmail.com>
2021-03-03 21:02:41 +0100

    Added null db

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,6 +1,4 @@
-unless ARGV.include? "assets:precompile"
-  ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
-  ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
-  ForestLiana.application_url = Rails.application.secrets.forest_application_url
-  ForestLiana.forest_client_id = Rails.application.secrets.forest_client_id
-end
+ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
+ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
+ForestLiana.application_url = Rails.application.secrets.forest_application_url
+ForestLiana.forest_client_id = Rails.application.secrets.forest_client_id
