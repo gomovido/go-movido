d4e03e77af1121f3b524fdfa6882d0e11bbef598
Yoni Touboul <yonitouboul@hotmail.com>
2021-03-03 20:48:08 +0100

    Skip liana forest during assets precompile

--- config/initializers/forest_liana.rb
+++ config/initializers/forest_liana.rb
@@ -1,4 +1,6 @@
-ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
-ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
-ForestLiana.application_url = Rails.application.secrets.forest_application_url
-ForestLiana.forest_client_id = Rails.application.secrets.forest_client_id
+unless ARGV.include? "assets:precompile"
+  ForestLiana.env_secret = Rails.application.secrets.forest_env_secret
+  ForestLiana.auth_secret = Rails.application.secrets.forest_auth_secret
+  ForestLiana.application_url = Rails.application.secrets.forest_application_url
+  ForestLiana.forest_client_id = Rails.application.secrets.forest_client_id
+end
