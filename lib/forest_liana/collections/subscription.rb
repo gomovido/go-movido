f973bd99bdf8e41a92821c3fc515bfc0be4f6b6a
Yoni Touboul <yonitouboul@hotmail.com>
2021-03-08 10:33:39 +0100

    Support polymorphic Forest Admin

--- lib/forest_liana/collections/subscription.rb
+++ lib/forest_liana/collections/subscription.rb
@@ -3,5 +3,12 @@ class Forest::Subscription
 
   collection :Subscription
 
+  belongs_to :wifi, reference: 'Wifi.id' do
+    object.product if object.product_type == 'Wifi'
+  end
+  belongs_to :mobile, reference: 'Mobile.id' do
+    object.product if object.product_type == 'Mobile'
+  end
+
   action 'Activate subscription'
 end
