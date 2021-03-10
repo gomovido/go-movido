a0cfebe645aded955107ac7f3dd8642049d7c02a
jordan demmouche <jordand75020@hotmail.fr>
2021-03-10 18:23:17 +0100

    Flats index spec

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,5 +1,14 @@
 class ProvidersController < ApplicationController
   def index
-    @flats = UniaccoApiService.new(city_code: params[:query]).list_flats
+    @uniacco_flats = UniaccoApiService.new(city_code: params[:query]).list_flats
+    if @uniacco_flats[:status] == 200
+      @uniacco_flats = @uniacco_flats[:payload]
+    else
+      @uniacco_flats = []
+    end
+    @homelike_flats = []
+    @uniplaces_flats = []
+    @flats = @homelike_flats + @uniplaces_flats + @uniacco_flats
+    redirect_to real_estate_landing_path if @flats.flatten.blank?
   end
 end
