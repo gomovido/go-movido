3fa2d14b9e41361cc1d558a92a026ed89b75f491
jordan demmouche <jordand75020@hotmail.fr>
2021-03-15 12:43:22 +0100

    Landing page & Search form

--- app/javascript/controllers/flats_controller.js
+++ app/javascript/controllers/flats_controller.js
@@ -3,9 +3,10 @@ import { mapBoxAutocomplete } from '../packs/mapbox';
 
 export default class extends Controller {
   static targets = ["geocoder"]
+  static values = { locale: String }
 
   connect() {
-    mapBoxAutocomplete(this.geocoderTarget)
+    mapBoxAutocomplete(this.geocoderTarget, this.localeValue)
     this.geocoderTarget.querySelector('input').setAttribute("name", "location")
   }
 }
--- app/javascript/packs/mapbox.js
+++ app/javascript/packs/mapbox.js
@@ -2,14 +2,23 @@ import mapboxgl from 'mapbox-gl';
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 
-export function mapBoxAutocomplete(element) {
+export function mapBoxAutocomplete(element, localevalue) {
   mapboxgl.accessToken = process.env.MAPBOX_API_KEY;
   const geocoder = new MapboxGeocoder({
     accessToken: mapboxgl.accessToken,
     autocomplete: true,
     types: 'region,postcode,district,place,locality,neighborhood,address,poi',
     countries: 'FR,GB',
-    language: 'en'
+    language: 'en',
+    placeholder: setPlaceholder(localevalue)
   });
   return geocoder.addTo(element);
+}
+
+function setPlaceholder(localevalue) {
+  if (localevalue === 'en') {
+    return 'Where are you going?'
+  } else if (localevalue === 'fr') {
+    return 'Où allez-vous?'
+  }
 }
