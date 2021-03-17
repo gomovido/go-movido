3ba599148091277214acb91cf18e83260a7aed2a
jordan demmouche <jordand75020@hotmail.fr>
2021-03-17 13:58:28 +0100

    Facilities icon on flats index

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -7,6 +7,30 @@ module FacilitiesHelper
       '<i class="fad fa-oven"></i>'.html_safe
     when 'gym'
       '<i class="far fa-dumbbell"></i>'.html_safe
+    when 'bed'
+      '<i class="far fa-bed"></i>'.html_safe
+    when 'shower'
+      '<i class="fad fa-shower"></i>'.html_safe
+    when 'kitchen'
+      '<i class="far fa-refrigerator"></i>'.html_safe
+    when 'room-type'
+      '<i class="fad fa-key"></i>'.html_safe
+    when 'wardrobe'
+      '<i class="fad fa-tshirt"></i>'.html_safe
+    when 'endstudy-desk'
+      '<i class="fal fa-lamp-desk"></i>'.html_safe
+    when 'window'
+      '<i class="far fa-blinds-open"></i>'.html_safe
+    when 'room-cleaning'
+      '<i class="far fa-vacuum"></i>'.html_safe
+    when 'uf-heating'
+      '<i class="fad fa-heat"></i>'.html_safe
+    when 'room-size'
+      '<i class="fal fa-cube"></i>'.html_safe
+    when 'study-desk'
+      '<i class="fal fa-chair-office"></i>'.html_safe
+    when 'double-bed'
+      '<i class="far fa-people-arrows"></i>'.html_safe
     end
   end
 end
