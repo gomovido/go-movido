e869c7952782582bfd79eb3b32c7d310819af2ab
Yoni Touboul <yonitouboul@hotmail.com>
2021-03-29 18:42:16 +0200

    Refacto js controllers

--- app/javascript/controllers/flats_controller.js
+++ app/javascript/controllers/flats_controller.js
@@ -1,12 +1,87 @@
-import { Controller } from "stimulus";
-import { mapBoxAutocomplete } from '../packs/mapbox';
+import { Controller } from "stimulus"
+import Rails from "@rails/ujs";
+import StimulusReflex from 'stimulus_reflex';
 
 export default class extends Controller {
-  static targets = ["geocoder"]
-  static values = { locale: String }
+  static targets = ["entries", "pagination", "spinner", "form", "spinner"]
+
+  initialize() {
+    this.intersectionObserver = new IntersectionObserver(entries => this.processIntersectionEntries(entries), {rootMargin: '100px'})
+  }
 
   connect() {
-    mapBoxAutocomplete(this.geocoderTarget, this.localeValue)
-    this.geocoderTarget.querySelector('input').setAttribute("name", "location")
+    this.intersectionObserver.observe(this.paginationTarget)
+    StimulusReflex.register(this)
+  }
+
+  disconnect() {
+    this.intersectionObserver.unobserve(this.paginationTarget)
+  }
+
+
+  afterReflex(e) {
+    let old_url = document.querySelector("a[rel='next']").getAttribute("href");
+    document.querySelector("a[rel='next']").href = old_url.replace(/.$/,"2");
+    this.hideLoading(this.spinnerTarget, this.entriesTarget)
+    document.querySelector('.dates_dates').classList.remove('disabled');
+  }
+
+  filterByFacility(e) {
+    const dateValue = document.getElementById('date-input').value
+    this.triggerLoading(this.spinnerTarget, this.entriesTarget)
+    this.stimulate('flatReflex#filter', this.formTarget, dateValue )
+  }
+
+  filterByDates(e) {
+    if (e.currentTarget.value.split(' ').length === 3) {
+      document.querySelector('.dates_dates').classList.add('disabled');
+      this.triggerLoading(this.spinnerTarget, this.entriesTarget)
+      this.stimulate('Flat#filter', e.currentTarget.value);
+    }
+  }
+
+  triggerLoading(spinner, wrapper) {
+    spinner.classList.remove('d-none');
+    spinner.classList.add('middle');
+    wrapper.classList.add('opacity');
+  }
+
+  hideLoading(spinner, wrapper) {
+    spinner.classList.add('d-none')
+    spinner.classList.remove('middle');
+    wrapper.classList.remove('opacity');
+  }
+
+  processIntersectionEntries(entries) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && !document.querySelector('.flats-wrapper').classList.contains('opacity')) {
+        this.loadMore()
+      }
+    })
+  }
+
+  loadMore() {
+    let next_page = this.paginationTarget.querySelector("a[rel='next']");
+    if (next_page == null) {
+      this.spinnerTarget.classList.add('d-none');
+      return
+    }
+    this.spinnerTarget.classList.remove('d-none');
+    let url = next_page.href
+    Rails.ajax({
+      type: 'GET',
+      url: url,
+      dataType: 'json',
+      success: (data) => {
+        if (data.entries) {
+          this.spinnerTarget.classList.add('d-none')
+          this.entriesTarget.insertAdjacentHTML('beforeend', data.entries)
+          this.paginationTarget.innerHTML = data.pagination
+        } else {
+          this.paginationTarget.innerHTML = data.pagination
+          this.loadMore()
+        }
+      }
+    })
   }
 }
--- app/reflexes/flat_reflex.rb
+++ app/reflexes/flat_reflex.rb
@@ -12,17 +12,18 @@ class FlatReflex < ApplicationReflex
     end
     @location = params['location']
     @type = params['type']
-    start_date = date.split[0].to_date
+    start_date = date.split[0]
     properties_codes = Rails.cache.read(:codes)
     @pagy, properties = pagy_array(properties_codes.split(','))
     response = UniaccoApiService.new(properties: properties, location: @location, active_filters: @active_filters, start_date: start_date).avanced_list_flats
     return if response[:status] != 200
+
     @flats = response[:flats]
     @other_flats = response[:recommandations]
     Rails.cache.write(:recommandations, @other_flats.to_json, expires_in: 30.minutes)
     morph ".flats-card-wrapper", render(partial: "flats/#{@browser.device.mobile? ? 'mobile' : 'desktop'}/flats", locals: { flats: @flats, location: @location, type: @type }, pagination: view_context.pagy_nav(@pagy))
     morph ".clear-filters", render(partial: "flats/#{@browser.device.mobile? ? 'mobile' : 'desktop'}/clear_filters", locals: { active_filters: @active_filters, location: @location, type: @type }) if @active_filters.present?
-    Rails.cache.write(:start_date, start_date.strftime, expires_in: 30.minutes)
+    Rails.cache.write(:start_date, start_date, expires_in: 30.minutes)
     Rails.cache.write(:filters, @active_filters.to_json, expires_in: 30.minutes) if @active_filters.present?
   end
 
