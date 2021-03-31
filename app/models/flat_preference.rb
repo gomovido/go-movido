07ca6fed346b5d0347922cd32a6709bbc7c844dc
Yoni Touboul <yonitouboul@hotmail.com>
2021-03-31 17:44:30 +0200

    refacto filters method in service

--- app/models/flat_preference.rb
+++ app/models/flat_preference.rb
@@ -4,4 +4,16 @@ class FlatPreference < ApplicationRecord
   def active?
     range_min_price || range_max_price || dishwasher || microwave
   end
+
+  def min_price
+    range_min_price || start_min_price
+  end
+
+  def max_price
+    range_max_price || start_max_price
+  end
+
+  def facilities
+    attributes.filter_map { |k, v| k if v == true }
+  end
 end
