b0f99ff1c0ba1a94db50a35253de33f04cda9635
Yoni Touboul <yonitouboul@hotmail.com>
2021-03-31 17:29:46 +0200

    Refacto

--- app/services/uniacco_api_service.rb
+++ app/services/uniacco_api_service.rb
@@ -18,8 +18,8 @@ class UniaccoApiService
       response['properties'] << JSON.parse(Net::HTTP.get(uri))['properties']
     end
     payload = response['properties'].flatten
-    min_price = payload.sort_by{|k| k['min_price']}[0]['min_price']
-    max_price = payload.sort_by{|k| k['max_price']}[-1]['max_price']
+    min_price = payload.min_by { |k| k['min_price'] }['min_price']
+    max_price = payload.max_by { |k| k['max_price'] }['max_price']
     if response && (response['title'] == 'NOT_FOUND')
       { error: 'NOT_FOUND', status: 404, flats: nil }
     else
@@ -45,16 +45,14 @@ class UniaccoApiService
 
   def filters(flats, flat_preference_id)
     flat_preference = FlatPreference.find(flat_preference_id)
-    start_date = flat_preference.start_date
     range_min_price = flat_preference.range_min_price || flat_preference.start_min_price
     range_max_price = flat_preference.range_max_price || flat_preference.start_max_price
     preferences = flat_preference.attributes.filter_map { |k, v| k if v == true }
     flats.filter do |flat|
       availability_date = flat[:details]['configs'][0]['subconfigs'][0]['available_from'].to_date
       facilities = flat[:apartment_facilities].map { |facility| facility['kind'].tr('-', '_') }
-      flat_min_price = flat[:details]['min_price'].to_i
-      flat_max_price = flat[:details]['max_price'].to_i
-      match_date_and_pricing = availability_date <= start_date && flat_min_price >= range_min_price && flat_max_price <= range_max_price
+      average_price = (flat[:details]['min_price'].to_i + flat[:details]['max_price'].to_i) / 2
+      match_date_and_pricing = availability_date <= flat_preference.start_date && average_price.between?(range_min_price, range_max_price)
       filters_condition = match_date_and_pricing && preferences.present? && (preferences - facilities).empty?
       no_filters_condition = match_date_and_pricing && preferences.blank?
       flat if filters_condition || no_filters_condition
@@ -72,10 +70,10 @@ class UniaccoApiService
   def flat
     uri = URI("https://uniacco.com/api/v1/uk/#{@location}/#{@property}")
     response = JSON.parse(Net::HTTP.get(uri))
-    if response && (response['title'] != 'NOT_FOUND')
-      hash = { code: @property, details: response, images: response['images'], facilities: response['facilities'], apartment_facilities: response['apartment_facilities'] }
-      { error: nil, status: 200, payload: hash }
-    end
+    return unless response && response['title'] != 'NOT_FOUND'
+
+    hash = { code: @property, details: response, images: response['images'], facilities: response['facilities'], apartment_facilities: response['apartment_facilities'] }
+    { error: nil, status: 200, payload: hash }
   end
 
   def check_and_return_city_code
