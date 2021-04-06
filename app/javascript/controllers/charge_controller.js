8a082016eb477265685eb4e604701fb35e0a39c9
jordan demmouche <jordand75020@hotmail.fr>
2021-04-06 17:24:18 +0200

    Add dotenv with staging.js

--- app/javascript/controllers/charge_controller.js
+++ app/javascript/controllers/charge_controller.js
@@ -4,6 +4,8 @@ export default class extends Controller {
 
   connect() {
     const stripe = Stripe(process.env.STRIPE_PUBLISHABLE_KEY);
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.STRIPE_PUBLISHABLE_KEY);
     const elements = stripe.elements({locale: document.getElementById('payment-form').dataset.locale});
 
     const style = {
