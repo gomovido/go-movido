f9fe497874c4875b70f8b3e6493f760ac51588e5
jordan demmouche <jordand75020@hotmail.fr>
2021-04-07 18:39:16 +0200

    Restaure providers#index with fake pricing

--- app/services/uniplaces_api_service.rb
+++ app/services/uniplaces_api_service.rb
@@ -10,11 +10,14 @@ class UniplacesApiService
     uri = URI("https://api.staging-uniplaces.com/v1/offers/#{@country.upcase}-#{@location}?page=#{@page}")
     response = HTTParty.get(uri, :headers => {"X-Api-Key" => "#{ENV['UNIPLACES_API_KEY']}", "Content-Type" => "application/json"})
     payload = response['data']
+    prices = get_pricing(payload)
     if payload
       {
         error: nil,
         status: 200,
         flats: payload,
+        min_price: 50,
+        max_price: 2500,
         recommandations:  [],
         codes: [],
         total_pages: response['meta']['total_page_number']
@@ -22,5 +25,14 @@ class UniplacesApiService
     end
   end
 
+
+  def get_pricing(payload)
+    pricing_rules = { monthly:  4, fortnight: 2, nightly: 0.7, weekly: 1}
+    prices = payload.map do |p|
+      price = p['attributes']['accommodation_offer']['price']['amount']
+      billing = p['attributes']['accommodation_offer']['contract_type']
+      ((price / pricing_rules[billing.to_sym]) / 100).round(0)
+    end
+  end
 
 end
