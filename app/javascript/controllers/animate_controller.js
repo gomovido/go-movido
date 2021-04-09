4c82e461e3d1716a7b269d97bc73826685aaf7cd
Yoni Touboul <yonitouboul@hotmail.com>
2021-04-09 12:43:48 +0200

    [skip travis] fix service card bottom line

--- app/javascript/controllers/animate_controller.js
+++ app/javascript/controllers/animate_controller.js
@@ -3,7 +3,18 @@ import ApplicationController from './application_controller'
 export default class extends ApplicationController {
   static targets = ['container', 'leftArrow', 'rightArrow', 'card']
 
-  displayCard(event) {
+  displayServiceCard(event) {
+    const card = this.cardTargets.find(card => card.dataset.serviceName === event.currentTarget.dataset.serviceName);
+    card.classList.toggle('open-card')
+    card.querySelector('.top-line').classList.toggle('open')
+    card.getElementsByClassName("card-link")[0].classList.toggle('d-none')
+    let icon = card.getElementsByClassName("far")[0]
+    if (icon) {
+      icon.classList.toggle('fa-plus')
+      icon.classList.toggle('fa-window-minimize')
+    }
+  }
+  displayExplanationCard(event) {
     const card = this.cardTargets.find(card => card.dataset.serviceName === event.currentTarget.dataset.serviceName);
     card.classList.toggle('open-card')
     card.getElementsByClassName("card-link")[0].classList.toggle('d-none')
