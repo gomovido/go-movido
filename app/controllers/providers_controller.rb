54345c3b2f12ddcfaa8b31c50068f75f72a64048
Yoni Touboul <yonitouboul@hotmail.com>
2021-04-12 12:01:22 +0200

    Added pictures in unplaces

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -4,7 +4,7 @@ class ProvidersController < ApplicationController
     @flat_preference = current_user.flat_preference
     @uniplaces_payload = uniplaces_flats(@flat_preference.location, @flat_preference.country)
     @uniacco_payload = uniacco_flats(@flat_preference.location)
-    @flats = @uniacco_payload[:flats] + @uniplaces_payload[:flats] if update_preferences(@uniplaces_payload, @uniacco_payload, @flat_preference)
+    @flats = @uniacco_payload[:flats] + @uniplaces_payload[:flats]
     unless @flats or !@flats.blank?
       flash[:alert] = 'Please type another location'
       redirect_to real_estate_path
@@ -22,12 +22,4 @@ class ProvidersController < ApplicationController
     return unless payload[:status] == 200 && current_user.flat_preference.update(codes: payload[:codes])
     payload
   end
-
-  def update_preferences(uniplaces_payload, uniacco_payload, preferences)
-    min_price = [uniplaces_payload[:max_price], uniplaces_payload[:min_price], uniacco_payload[:max_price], uniacco_payload[:max_price]].min
-    max_price = [uniplaces_payload[:max_price], uniplaces_payload[:min_price], uniacco_payload[:max_price], uniacco_payload[:max_price]].max
-    preferences.update(start_min_price: min_price, start_max_price: max_price)
-  end
-
-
 end
--- app/services/uniplaces_api_service.rb
+++ app/services/uniplaces_api_service.rb
@@ -11,14 +11,11 @@ class UniplacesApiService
     uri = URI("https://api.staging-uniplaces.com/v1/offers/#{@country.upcase}-#{@location}?page=#{@page}")
     response = HTTParty.get(uri, :headers => {"X-Api-Key" => "#{ENV['UNIPLACES_API_KEY']}", "Content-Type" => "application/json"})
     payload = response['data']
-    prices = get_pricing(payload)
     if payload
       {
         error: nil,
         status: 200,
         flats: payload,
-        min_price: 50,
-        max_price: 2500,
         recommandations:  [],
         codes: [],
         total_pages: response['meta']['total_page_number']
@@ -26,33 +23,21 @@ class UniplacesApiService
     end
   end
 
-
   def list_flat
     headers = { API_KEY: ENV['UNIPLACES_API_KEY'] }
     uri = URI("https://api.staging-uniplaces.com/v1/offer/#{@code}")
     response = HTTParty.get(uri, :headers => {"X-Api-Key" => "#{ENV['UNIPLACES_API_KEY']}", "Content-Type" => "application/json"})
     payload = response
+    #TODO
     if payload
       {
         error: nil,
         status: 200,
         flat: payload,
-        min_price: 50,
-        max_price: 2500,
         recommandations:  [],
         codes: [],
         total_pages: 1
       }
     end
   end
-
-  def get_pricing(payload)
-    pricing_rules = { monthly:  4, fortnight: 2, nightly: 0.7, weekly: 1}
-    prices = payload.map do |p|
-      price = p['attributes']['accommodation_offer']['price']['amount']
-      billing = p['attributes']['accommodation_offer']['contract_type']
-      ((price / pricing_rules[billing.to_sym]) / 100).round(0)
-    end
-  end
-
 end
