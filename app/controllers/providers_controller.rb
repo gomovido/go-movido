952989449f2d7c27ab48266c072f1a95a9a15895
jordan demmouche <jordand75020@hotmail.fr>
2021-04-14 17:06:59 +0200

    refactor providers controller & uniacco api service method list flats

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -5,29 +5,21 @@ class ProvidersController < ApplicationController
     clear_filters
     @uniplaces_payload = uniplaces_flats(@flat_preference.location, @flat_preference.country)
     @uniacco_payload = uniacco_flats(@flat_preference.location)
-    @flats = @uniacco_payload + @uniplaces_payload
-    return if @flats.present?
+    @uniacco_flats = @uniacco_payload[:flats]
+    @uniplaces_flats = @uniplaces_payload[:flats]
+    @count = @uniacco_payload[:count] + @uniplaces_payload[:count]
+    return if (@uniacco_flats + @uniplaces_flats).present?
 
     flash[:alert] = 'Please type another location'
     redirect_to real_estate_path
   end
 
   def uniplaces_flats(location, country)
-    payload = UniplacesApiService.new(city_code: location, country: country, flat_preference_id: current_user.flat_preference.id, page: 1).list_flats
-    if payload && payload[:status] == 200
-      payload[:flats]
-    else
-      []
-    end
+    UniplacesApiService.new(city_code: location, country: country, flat_preference_id: current_user.flat_preference.id, page: 1).list_flats
   end
 
   def uniacco_flats(location)
-    payload = UniaccoApiService.new(city_code: location).list_flats
-    if payload[:status] == 200 && current_user.flat_preference.update(codes: payload[:codes])
-      payload[:flats]
-    else
-      []
-    end
+    UniaccoApiService.new(city_code: location).list_flats
   end
 
   def clear_filters
--- app/services/uniacco_api_service.rb
+++ app/services/uniacco_api_service.rb
@@ -10,118 +10,151 @@ class UniaccoApiService
   end
 
   def list_flats
-    uri = URI("https://uniacco.com/api/v1/cities/#{@city_code}/properties?page=1&sortBy=relevance")
+    uri = URI("https://uniacco.com/api/v1/cities/#{@city_code}/properties?sortBy=relevance")
     response = JSON.parse(Net::HTTP.get(uri))
-    i = 1
-    while i < response['pages'].to_i
-      i += 1
-      uri = URI("https://uniacco.com/api/v1/cities/#{@city_code}/properties?page=#{i}&sortBy=relevance")
-      response['properties'] << JSON.parse(Net::HTTP.get(uri))['properties']
-    end
-    payload = response['properties'].flatten
     if response && (response['title'] == 'NOT_FOUND')
       {
         error: 'NOT_FOUND',
         status: 404,
-        flats: nil
+        flats: [],
+        recommandations: [],
+        total_pages: 0,
+        count: 0
+
       }
-    else
-      recommandations = payload.first(12).map do |flat|
-        {
-          code: flat['code'],
-          image: flat['images'][0]['url'],
-          price: flat['disp_price'],
-          billing: flat['billing'],
-          name: flat['name']
-        }.to_json
-      end
+    elsif response && (response['title'] != 'NOT_FOUND')
       {
         error: nil,
         status: 200,
-        flats: payload,
-        recommandations: recommandations,
-        codes: codes(payload)
+        flats: response['properties'],
+        recommandations: recommandations(response['properties']),
+        total_pages: response['pages'],
+        count: response['count']
       }
     end
   end
 
-  def avanced_list_flats
-    flat_preference = FlatPreference.find(@flat_preference_id)
-    array = @properties.map do |property|
-      uri = URI("https://uniacco.com/api/v1/#{flat_preference.country == 'fr' ? 'france' : 'uk'}/#{flat_preference.location}/#{property}")
-      response = JSON.parse(Net::HTTP.get(uri))
-      next unless response && (response['title'] != 'NOT_FOUND')
 
+  def recommandations(properties)
+    properties.first(5).map do |flat|
       {
-        code: property,
-        details: response,
-        images: response['images'],
-        facilities: response['facilities'],
-        apartment_facilities: response['apartment_facilities'],
-        community_facilities: response['community_facilities']
-      }
-    end
-    return if array.blank?
-
-    flats = filters(array, flat_preference.move_in, flat_preference.min_price, flat_preference.max_price, flat_preference.facilities)
-    {
-      error: nil,
-      status: 200,
-      flats: flats,
-      recommandations: recommandations(flats)
-    }
-  end
-
-  def filters(flats, start_date, min_price, max_price, facilities)
-    flats.filter do |flat|
-      flat_start_date = flat[:details]['configs'][0]['subconfigs'][0]['available_from'].to_date
-      flat_facilities = flat[:apartment_facilities].map { |facility| facility['kind'].tr('-', '_') }
-      flat_price = (flat[:details]['min_price'].to_i + flat[:details]['max_price'].to_i) / 2
-      match_date_and_pricing = flat_start_date <= start_date && flat_price.between?(min_price, max_price)
-      flat if match_date_and_pricing && ((facilities.present? && (facilities - flat_facilities).empty?) || facilities.blank?)
-    end
-  end
-
-  def recommandations(flats)
-    flats.first(5).map do |flat|
-      {
-        code: flat[:code],
-        image: flat[:images][0]['url'],
-        price: flat[:details]['disp_price'],
-        billing: flat[:details]['billing'],
-        name: flat[:details]['name']
+        code: flat['code'],
+        image: flat['images'][0]['url'],
+        price: flat['disp_price'],
+        billing: flat['billing'],
+        name: flat['name']
       }.to_json
     end
   end
 
-  def codes(flats)
-    flats.map { |flat| flat['code'] }
-  end
+  # def list_flats
+  #   uri = URI("https://uniacco.com/api/v1/cities/#{@city_code}/properties?page=1&sortBy=relevance")
+  #   response = JSON.parse(Net::HTTP.get(uri))
+  #   i = 1
+  #   while i < response['pages'].to_i
+  #     i += 1
+  #     uri = URI("https://uniacco.com/api/v1/cities/#{@city_code}/properties?page=#{i}&sortBy=relevance")
+  #     response['properties'] << JSON.parse(Net::HTTP.get(uri))['properties']
+  #   end
+  #   payload = response['properties'].flatten
+  #   if response && (response['title'] == 'NOT_FOUND')
+  #     {
+  #       error: 'NOT_FOUND',
+  #       status: 404,
+  #       flats: nil
+  #     }
+  #   else
+  #     recommandations = payload.first(12).map do |flat|
+  #       {
+  #         code: flat['code'],
+  #         image: flat['images'][0]['url'],
+  #         price: flat['disp_price'],
+  #         billing: flat['billing'],
+  #         name: flat['name']
+  #       }.to_json
+  #     end
+  #     {
+  #       error: nil,
+  #       status: 200,
+  #       flats: payload,
+  #       recommandations: recommandations,
+  #       codes: codes(payload)
+  #     }
+  #   end
+  # end
 
-  def flat
-    flat_preference = FlatPreference.find(@flat_preference_id)
-    uri = URI("https://uniacco.com/api/v1/#{flat_preference.country == 'fr' ? 'france' : 'uk'}/#{@location}/#{@property}")
-    response = JSON.parse(Net::HTTP.get(uri))
-    return unless response && response['title'] != 'NOT_FOUND'
+  # def avanced_list_flats
+  #   flat_preference = FlatPreference.find(@flat_preference_id)
+  #   array = @properties.map do |property|
+  #     uri = URI("https://uniacco.com/api/v1/#{flat_preference.country == 'fr' ? 'france' : 'uk'}/#{flat_preference.location}/#{property}")
+  #     response = JSON.parse(Net::HTTP.get(uri))
+  #     next unless response && (response['title'] != 'NOT_FOUND')
 
-    {
-      error: nil,
-      status: 200,
-      payload: {
-        code: @property,
-        details: response,
-        images: response['images'],
-        facilities: response['facilities'],
-        apartment_facilities: response['apartment_facilities'],
-        community_facilities: response['community_facilities']
-      }
-    }
-  end
+  #     {
+  #       code: property,
+  #       details: response,
+  #       images: response['images'],
+  #       facilities: response['facilities'],
+  #       apartment_facilities: response['apartment_facilities'],
+  #       community_facilities: response['community_facilities']
+  #     }
+  #   end
+  #   return if array.blank?
 
-  def check_and_return_city_code
-    uri = URI("https://uniacco.com/api/v1/countries/#{@country == 'fr' ? 'france' : 'uk'}/cities")
-    response = JSON.parse(Net::HTTP.get(uri))
-    city = response['cities'].find { |city_uniacco| city_uniacco if @location.include?(city_uniacco['code']) }
-    city['code'] unless city.nil?
-  end
+  #   flats = filters(array, flat_preference.move_in, flat_preference.min_price, flat_preference.max_price, flat_preference.facilities)
+  #   {
+  #     error: nil,
+  #     status: 200,
+  #     flats: flats,
+  #     recommandations: recommandations(flats)
+  #   }
+  # end
+
+  # def filters(flats, start_date, min_price, max_price, facilities)
+  #   flats.filter do |flat|
+  #     flat_start_date = flat[:details]['configs'][0]['subconfigs'][0]['available_from'].to_date
+  #     flat_facilities = flat[:apartment_facilities].map { |facility| facility['kind'].tr('-', '_') }
+  #     flat_price = (flat[:details]['min_price'].to_i + flat[:details]['max_price'].to_i) / 2
+  #     match_date_and_pricing = flat_start_date <= start_date && flat_price.between?(min_price, max_price)
+  #     flat if match_date_and_pricing && ((facilities.present? && (facilities - flat_facilities).empty?) || facilities.blank?)
+  #   end
+  # end
+
+  # def recommandations(flats)
+  #   flats.first(5).map do |flat|
+  #     {
+  #       code: flat[:code],
+  #       image: flat[:images][0]['url'],
+  #       price: flat[:details]['disp_price'],
+  #       billing: flat[:details]['billing'],
+  #       name: flat[:details]['name']
+  #     }.to_json
+  #   end
+  # end
+
+  # def codes(flats)
+  #   flats.map { |flat| flat['code'] }
+  # end
+
+  # def flat
+  #   flat_preference = FlatPreference.find(@flat_preference_id)
+  #   uri = URI("https://uniacco.com/api/v1/#{flat_preference.country == 'fr' ? 'france' : 'uk'}/#{@location}/#{@property}")
+  #   response = JSON.parse(Net::HTTP.get(uri))
+  #   return unless response && response['title'] != 'NOT_FOUND'
+
+  #   {
+  #     error: nil,
+  #     status: 200,
+  #     payload: {
+  #       code: @property,
+  #       details: response,
+  #       images: response['images'],
+  #       facilities: response['facilities'],
+  #       apartment_facilities: response['apartment_facilities'],
+  #       community_facilities: response['community_facilities']
+  #     }
+  #   }
+  # end
+
+
 end
