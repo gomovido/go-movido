2dec2019b4e33052af5b448dabccd5598edd21e5
Yoni Touboul <yonitouboul@hotmail.com>
2021-04-16 12:59:39 +0200

    New providers design

--- app/models/flat_preference.rb
+++ app/models/flat_preference.rb
@@ -1,6 +1,7 @@
 class FlatPreference < ApplicationRecord
   attr_accessor :coordinates, :date_range
 
+  TYPES = ["student_housing", "entire_flat", "upscale_housing", "flatshare"]
   FACILITIES = {
     student_housing: ["kitchen", "wifi", "gym", "room_cleaning", "laundry"],
     entire_flat: ["wi_fi", "unfurnished", "accessibility", "elevator", "outdoor_area", "washing_machine"]
