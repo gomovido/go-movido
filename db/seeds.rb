6c1b6de6871f1229cbc74ebbb7c30433b45fca7a
jordan demmouche <jordand75020@hotmail.fr>
2021-05-04 15:29:31 +0200

    Final schema matching production

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,3 @@ Category.find_by(name: 'utilities').update(sort_id: 5)
 Category.find_by(name: 'transportation').update(sort_id: 6)
 Category.find_by(name: 'gym').update(sort_id: 7)
 Category.find_by(name: 'community').update(sort_id: 8)
-Mobile.all.each do |mobile|
-  mobile.product_features.each_with_index do |pf, index|
-    pf.update_columns(order: index)
-  end
-end
-Wifi.all.each do |wifi|
-  wifi.product_features.each_with_index do |pf, index|
-    pf.update_columns(order: index)
-  end
-end
