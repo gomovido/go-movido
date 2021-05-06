de2898a4591e0323f03ca7efa0eecb1995de5b29
Yoni Touboul <yonitouboul@hotmail.com>
2021-05-06 17:01:44 +0200

    Update stripe products and skus

--- app/services/stripe_api_product_service.rb
+++ app/services/stripe_api_product_service.rb
@@ -2,6 +2,7 @@ class StripeApiProductService
   def initialize(params)
     @product_id = params[:product_id]
     @type = params[:type]
+    @sku = params[:sku]
   end
 
   def proceed
@@ -11,35 +12,61 @@ class StripeApiProductService
   end
 
   def create_product(product)
+    Stripe::Product.create({
+                             type: 'good',
+                             name: "#{product.company.name} #{product.name}",
+                             images: [product.company.logo_url]
+                           })
+  rescue Stripe::StripeError => e
+    e
+  end
+
+  def proceed_update
+    product = @type.constantize.find(@product_id)
+    update_product(product)
+    update_sku(product)
+  end
+
+  def update_product(product)
+    response = Stripe::Product.retrieve(Stripe::SKU.retrieve(@sku).product)
     begin
-      Stripe::Product.create({
-        type: 'good',
+      Stripe::Product.update(
+        response.id,
         name: "#{product.company.name} #{product.name}",
         images: [product.company.logo_url]
-      })
-    rescue Stripe::StripeError => error
-      error
+      )
+    rescue Stripe::StripeError => e
+      e
+    end
+  end
+
+  def update_sku(product)
+    price = product.category.name == 'wifi' ? 0 : product.sim_card_price_cents
+    begin
+      Stripe::SKU.update(
+        @sku,
+        price: price,
+        currency: product.country.currency,
+        image: product.company.logo_url
+      )
+    rescue Stripe::StripeError => e
+      e
     end
   end
 
   def create_sku(product, stripe_product_id)
-    if product.category.name == 'wifi'
-      price = 0
-    else
-      price = product.sim_card_price_cents
-    end
+    price = product.category.name == 'wifi' ? 0 : product.sim_card_price_cents
     begin
       sku = Stripe::SKU.create({
-        price: price,
-        currency: product.country.currency,
-        inventory: {type: 'infinite'},
-        product: stripe_product_id,
-        image: product.company.logo_url
-      })
+                                 price: price,
+                                 currency: product.country.currency,
+                                 inventory: { type: 'infinite' },
+                                 product: stripe_product_id,
+                                 image: product.company.logo_url
+                               })
       return { product_id: sku.id, error: nil }
     rescue Stripe::StripeError => e
       return { product_id: nil, error: e }
     end
   end
-
 end
