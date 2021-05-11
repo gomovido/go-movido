6461e755f5badd71a2b5cb61286dc31beb3aaaa2
Yoni Touboul <yonitouboul@hotmail.com>
2021-05-11 12:33:25 +0200

    Lint rubocop

--- app/models/flat_preference.rb
+++ app/models/flat_preference.rb
@@ -1,7 +1,7 @@
 class FlatPreference < ApplicationRecord
   attr_accessor :date_range
 
-  TYPES = ["student_housing", "entire_flat", "flatshare" , "upscale_housing" ]
+  TYPES = ["student_housing", "entire_flat", "flatshare", "upscale_housing"]
   FACILITIES = {
     student_housing: ["kitchen", "wifi", "gym", "room_cleaning", "laundry"],
     entire_flat: ["wi_fi", "unfurnished", "accessibility", "elevator", "outdoor_area", "washing_machine"],
