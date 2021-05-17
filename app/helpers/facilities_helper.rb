472bae2a9a1344f401024a8067984f60152f0e1c
Yoni Touboul <yonitouboul@hotmail.com>
2021-05-17 10:57:21 +0200

    [skip travis] Fix lorem ipsum descriptions

--- app/helpers/facilities_helper.rb
+++ app/helpers/facilities_helper.rb
@@ -6,10 +6,10 @@ module FacilitiesHelper
   }
 
   def split_description(description)
-    description ? "#{ActionView::Base.full_sanitizer.sanitize(description).first(200)}..." : 'Lorem ipsum dolor sit amet,  consectetur adipiscing elit consectetur adipiscing elit consectetur adipiscing elit...'
+    description ? "#{ActionView::Base.full_sanitizer.sanitize(description).first(200)}..." : 'No description available'
   end
 
   def full_description(description)
-    description ? ActionView::Base.full_sanitizer.sanitize(description).gsub(/\. /, '.<br><br>') : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras consectetur facilisis lorem. Proin sit amet viverra velit. Proin semper auctor convallis. Integer turpis leo, rutrum elementum scelerisque non, molestie non massa. Morbi commodo fermentum lectus, ut blandit nunc pretium sollicitudin. Aliquam nunc metus, rhoncus in purus sit amet, fermentum volutpat arcu. Mauris imperdiet nulla quis luctus molestie'
+    description ? ActionView::Base.full_sanitizer.sanitize(description).gsub(/\. /, '.<br><br>') : 'No description available'
   end
 end
