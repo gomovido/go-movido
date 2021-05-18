6d00e1e364b28a22ec7f92c8b301f07daf023151
jordan demmouche <jordand75020@hotmail.fr>
2021-05-18 14:03:06 +0200

    real estate skip_before_action authenticate user & redirect to providers path after sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,9 +36,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email first_name last_name])
-    devise_parameter_sanitizer.permit(:sign_in,
-                                      keys: %i[
-                                        email password password_confirmation
-                                      ])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password password_confirmation])
   end
 end
--- app/controllers/flat_preferences_controller.rb
+++ app/controllers/flat_preferences_controller.rb
@@ -1,22 +1,32 @@
 class FlatPreferencesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create]
   def new
     @flat_preference = FlatPreference.new
   end
 
   def create
-    @flat_preference = FlatPreference.find_by(user: current_user) || FlatPreference.new(user: current_user)
-    if flat_preference_params[:date_range].present?
-      @flat_preference.move_in = flat_preference_params[:date_range].split[0].to_date
-      @flat_preference.move_out = flat_preference_params[:date_range].split[-1].to_date
-    end
-    @flat_preference.coordinates = flat_preference_params[:coordinates].split(',')
-    @flat_preference.location = format_location_params(flat_preference_params[:coordinates])
-    @flat_preference.country = format_country_params(flat_preference_params[:coordinates])
-    if @flat_preference.save
-      redirect_to providers_path(@flat_preference.location)
+    if current_user.nil?
+      redirect_to new_user_registration_path(
+        request_type: 'real_estate',
+        move_in: flat_preference_params[:date_range].split[0].to_date,
+        move_out: flat_preference_params[:date_range].split[-1].to_date,
+        coordinates: flat_preference_params[:coordinates].split(',')
+        )
     else
-      flash[:alert] = 'Please type another location'
-      redirect_to real_estate_path
+      @flat_preference = FlatPreference.find_by(user: current_user) || FlatPreference.new(user: current_user)
+      if flat_preference_params[:date_range].present?
+        @flat_preference.move_in = flat_preference_params[:date_range].split[0].to_date
+        @flat_preference.move_out = flat_preference_params[:date_range].split[-1].to_date
+      end
+      @flat_preference.coordinates = flat_preference_params[:coordinates].split(',')
+      @flat_preference.location = format_location_params(flat_preference_params[:coordinates])
+      @flat_preference.country = format_country_params(flat_preference_params[:coordinates])
+      if @flat_preference.save
+        redirect_to providers_path(@flat_preference.location)
+      else
+        flash[:alert] = 'Please type another location'
+        redirect_to real_estate_path
+      end
     end
   end
 
