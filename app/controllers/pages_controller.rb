6aa3f9fb352e70a04aa678389715b31d921b8383
Yoni Touboul <yonitouboul@hotmail.com>
2021-05-20 19:36:17 +0200

    First step landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,5 +21,6 @@ class PagesController < ApplicationController
 
   def simplicity
     @lead = Lead.new
+    @companies_logo_urls = ['https://res.cloudinary.com/go-movido-com/image/upload/v1615298087/Company%20logos/revolut_sito9h.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621530529/Company%20logos/ee_dx8c5s.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1615298088/Company%20logos/transferwise_uauj31.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1615298088/Company%20logos/bouygues_telecom_wcouac.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621531336/Company%20logos/british-telecom_iowcnq.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621530529/Company%20logos/luko_v2mnki.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1615298089/Company%20logos/uniplaces_f1xg2s.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621530813/Company%20logos/uniacco_k1prjs.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621530529/Company%20logos/garant-me_xswnji.png']
   end
 end
