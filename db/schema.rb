acea9d35a3d239140494c2e01e42cb44e581c52a
jordan demmouche <jordand75020@hotmail.fr>
2021-06-03 12:46:04 +0200

    Model validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_134945) do
+ActiveRecord::Schema.define(version: 2021_06_03_103935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "sku"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "companies", force: :cascade do |t|
+    t.string "logo_url"
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "leads", force: :cascade do |t|
     t.string "email"
@@ -21,6 +37,29 @@ ActiveRecord::Schema.define(version: 2021_06_01_134945) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_leads_on_email", unique: true
+  end
+
+  create_table "product_details", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_product_details_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.bigint "company_id", null: false
+    t.bigint "category_id", null: false
+    t.string "name"
+    t.string "sku"
+    t.float "activation_price"
+    t.float "subscription_price"
+    t.string "currency"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["company_id"], name: "index_products_on_company_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -35,4 +74,7 @@ ActiveRecord::Schema.define(version: 2021_06_01_134945) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "product_details", "products"
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "companies"
 end
