77f146ccdb9ece8862c8088127f6e3fe2f6bc772
jordan demmouche <jordand75020@hotmail.fr>
2021-06-03 13:38:47 +0200

    Merge Models / forest admin schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_06_03_111842) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_06_03_111842) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-  
+
   create_table "companies", force: :cascade do |t|
     t.string "logo_url"
     t.string "name"
