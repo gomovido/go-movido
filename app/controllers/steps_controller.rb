9ef8d54a34c39c97674df2b06aadeb95ed818cb3
Yoni Touboul <yonitouboul@hotmail.com>
2021-06-07 17:20:07 +0200

    Simplicity flow tests finished

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -3,4 +3,8 @@ class StepsController < ApplicationController
     @user_preference = current_user.user_preference || UserPreference.new
     @messages = [{ content: "First of all, where are you moving to?", delay: 0 }]
   end
+
+  def conversion
+    @order = Order.find(params[:order_id])
+  end
 end
