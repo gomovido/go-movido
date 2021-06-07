118d592d0dcfdb97f398d8472a07d7cd88508f46
Yoni Touboul <yonitouboul@hotmail.com>
2021-06-07 13:12:28 +0200

    Form base to content container

--- app/javascript/controllers/animate_controller.js
+++ app/javascript/controllers/animate_controller.js
@@ -3,12 +3,12 @@ import StimulusReflex from 'stimulus_reflex';
 
 export default class extends Controller {
 
-  static targets = ["form", "message"]
+  static targets = ["contentContainer", "message"]
 
   connect() {
     StimulusReflex.register(this);
     this.messageTargets.forEach(message => message.classList.add('animate__animated', 'animate__fadeInLeft', `animate__delay-${message.dataset.delay}s`));
-    this.formTarget.classList.add('animate__animated', 'animate__fadeInLeft', `animate__delay-${this.formTarget.dataset.delay}s`)
+    this.contentContainerTarget.classList.add('animate__animated', 'animate__fadeInLeft', `animate__delay-${this.contentContainerTarget.dataset.delay}s`)
   }
 
   afterReflex() {
