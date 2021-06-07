18265be555d45cdaac9a26b03c8b1c9e68221e65
jordan demmouche <jordand75020@hotmail.fr>
2021-06-07 16:15:38 +0200

    Payment spec with payment resolved

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,20 @@
 class PaymentsController < ApplicationController
   def new
-    @order = Order.find(params[:order_id])
+    @order = Order.find_by(id: params[:order_id], user: current_user)
+    redirect_to congratulations_path(@order) if @order.is_pay
   end
 
   def create
-    response = StripeApiChargeService.new(stripe_token: params[:stripe_token], order_id: params[:order_id]).proceed_payment
+    stripe_token = params[:stripeToken]
+    order = Order.find(params[:order_id])
+    response = StripeApiChargeService.new(stripe_token: stripe_token, order_id: order.id).proceed_payment
     if response[:error].nil? && response[:stripe_charge]
-      raise
       charge = Charge.create(state: response[:stripe_charge].status, stripe_charge_id: response[:stripe_charge].id)
-      Order.find(params[:order_id]).update(state: 'succedeed', charge: charge)
-      flas[:notice] = 'Payment success!'
-      redirect_to congratulations_path
+      order.update(state: 'succeeded', charge: charge)
+      flash[:notice] = 'Payment success!'
+      redirect_to congratulations_path(order.id)
     else
+      order.update(state: 'payment_failed')
       flash[:alert] = response[:error]
       redirect_back(fallback_location: root_path)
     end
--- app/services/stripe_api_charge_service.rb
+++ app/services/stripe_api_charge_service.rb
@@ -15,7 +15,7 @@ class StripeApiChargeService
       })
     return { stripe_charge: stripe_charge, error: nil }
     rescue Stripe::StripeError => e
-      return { stripe_charge: nil, error: e }
+      return { stripe_charge: stripe_charge, error: e }
     end
   end
 end
