962546174c8a3c84f6ceee2c64637579aefd2824
jordan demmouche <jordand75020@hotmail.fr>
2021-06-08 17:39:28 +0200

    Payment spec

--- spec/features/payment_spec.rb
+++ spec/features/payment_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Payment via Stripe", type: :feature do
-  describe "User want to proceed payment", :selenium_chrome do
+  describe "User want to proceed payment", :headless_chrome do
     let(:user) { create(:user) }
     let!(:country) { create(:country, :fr) }
     let(:company) { create(:company, :mobile) }
     let(:category) { create(:category, :mobile) }
     let(:user_preference) { create(:user_preference, user: user, country: country) }
     let(:cart) { create(:cart, user_preference: user_preference) }
-    let(:order) { create(:order, user: user, state: 'pending_payment') }
+    let!(:order) { create(:order, user: user, state: 'pending_payment') }
     let!(:product) { create(:product, :mobile, country: country, company: company, category: category) }
     let!(:item) { create(:item, product: product, cart: cart, order: order) }
 
     before do
       login_as(user, scope: :user)
-      visit new_order_payment_path(order, locale: :en)
+      visit new_order_payment_path(order.id, locale: :en)
     end
 
     context 'when user proceed payment with valid card' do
@@ -48,7 +48,42 @@ RSpec.describe "Payment via Stripe", type: :feature do
           sleep 4
           order.reload
         end.to change { order.state}.to('succeeded')
-        .and change {order.billing.address}.to('57 Rue Sedaine, 75011 Paris, France')
+      end
+    end
+
+
+
+    context 'when user proceed payment with valid card' do
+      it "update the state of the order to succeeded" do
+        within("#payment-form") do
+          fill_in 'billing_address_mapbox', with: '57 rue sedaine paris'
+          sleep 1
+          find('li.active', match: :first).click
+        end
+        [
+          {
+            selector: "#card-number-element > div > iframe",
+            values: '4242 4242 4242 4242',
+            input: 'cardnumber'
+          },
+          {
+            selector: "#card-expiry-element > div > iframe",
+            values: '1234',
+            input: 'exp-date'
+          },
+          {
+            selector: "#card-cvc-element > div > iframe",
+            values: '123',
+            input: 'cvc'
+          }
+        ].each do |hash|
+          find_in_frame(hash[:selector], hash[:input], hash[:values])
+        end
+        expect do
+          click_button 'Complete payment'
+          sleep 4
+          order.reload
+        end.to change { order.billing.address}.to('Ezaezaeza')
       end
     end
 
@@ -58,7 +93,6 @@ RSpec.describe "Payment via Stripe", type: :feature do
           fill_in 'billing_address_mapbox', with: '57 rue sedaine paris'
           sleep 1
           find('li.active', match: :first).click
-          sleep 50
         end
         [
           {
