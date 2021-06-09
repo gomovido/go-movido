32306acc37d789b47c40abaca79f1f8f01e05569
Yoni Touboul <yonitouboul@hotmail.com>
2021-06-09 12:01:46 +0200

    Updated stripe button to be disabled on submit + refacto errors and viewe

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -8,5 +8,13 @@ class StepsController < ApplicationController
     @order = Order.find(params[:order_id])
     @shipping = @order.shipping || Shipping.new
     @messages = [{ content: "Now please enter your current home address details for the shipment of your Starter Pack", delay: 0 }]
+    redirect_to congratulations_path(@order) if @order.paid?
+  end
+
+  def payment
+    @order = Order.find_by(id: params[:order_id], user: current_user)
+    @billing = @order.billing || Billing.new
+    @messages = [{ content: "Thanks #{current_user.first_name}, now please enter your payment details to finalize the order of your Starter Pack", delay: 0 }]
+    redirect_to congratulations_path(@order) if @order.paid?
   end
 end
