297509b0aaf1a17fef078ad14caa3b87026a5a40
Yoni Touboul <yonitouboul@hotmail.com>
2021-06-11 15:33:24 +0200

    Refacto spec

--- spec/models/charge_spec.rb
+++ spec/models/charge_spec.rb
@@ -8,9 +8,7 @@ RSpec.describe Charge, type: :model do
   describe 'validations' do
     let(:charge) { build(:charge) }
 
-    %i[state].each do |field|
-      it { is_expected.to validate_presence_of(field) }
-    end
+    it { is_expected.to validate_presence_of(:state) }
 
     it 'saves successfully' do
       expect(charge.save).to eq(true)
