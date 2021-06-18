dfd04805c867f1e5968c14630f4a7d8432955572
jordan demmouche <jordand75020@hotmail.fr>
2021-06-18 14:14:25 +0200

    Sign in fixes white screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
   before_action :http_auth, unless: -> { @stimulus_reflex }
+  protect_from_forgery prepend: true
 
   private
 
