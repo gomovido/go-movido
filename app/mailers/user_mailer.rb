e0ee69de059f03527ff7b310d5fbc573a0114d39
jordan demmouche <jordand75020@hotmail.fr>
2021-06-18 17:19:13 +0200

    Congratulations email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,14 @@
 class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    @locale = params[:locale]
+    @locale = "en"
     mail(to: @user.email, subject: I18n.t('mail.welcome_email'))
+  end
+
+  def order_confirmed
+    @user = params[:user]
+    @order = params[:subscription]
+    @locale = "en"
+    mail(to: @user.email, subject: "✨ #{@user.first_name}, your Starter Pack is on its way !")
   end
 end
