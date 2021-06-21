2de3c1cfbc1a05dbfa0f4f74b49028ebd62ab0ea
Yoni Touboul <yonitouboul@hotmail.com>
2021-06-21 14:36:10 +0200

    Fix default date [skip travis]

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -4,15 +4,5 @@ export default class extends Flatpickr {
   connect() {
     this.config = { locale: { rangeSeparator: ' - ' }};
     super.connect();
-    let field = document.querySelector('.flatpickr-mobile');
-    if (field) {
-      field.addEventListener('change', e => {
-        if (field.value) {
-          field.placeholder = ''
-        } else {
-          field.placeholder = 'DD/MM/YYYY'
-        }
-      });
-    }
   }
 }
