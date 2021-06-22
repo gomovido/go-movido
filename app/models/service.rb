27c397af1f2c69755a83a4ac0eb7853eba00099c
jordan demmouche <jordand75020@hotmail.fr>
2021-06-22 18:25:20 +0200

    Add pricing calculator

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,4 +5,9 @@ class Service < ApplicationRecord
   has_many :user_services, dependent: :destroy
   has_many :user_preferences, through: :user_services
   belongs_to :category
+
+
+  def price(country)
+    (category.products.find_by(country: country).activation_price.to_d * 100).to_i
+  end
 end
