7a54495d30672acd2c3867e1a5b5aa81a42360b2
Yoni Touboul <yonitouboul@hotmail.com>
2021-06-23 18:28:44 +0200

    Refacto reflexes

--- app/reflexes/shipping_reflex.rb
+++ app/reflexes/shipping_reflex.rb
@@ -11,15 +11,18 @@ class ShippingReflex < ApplicationReflex
     @shipping = @order.shipping || Shipping.new
     @shipping.assign_attributes(shipping_params)
     @shipping.state = 'initiated'
-    if @shipping.save
-      @order.update(shipping: @shipping)
-      if current_user.user_preference.pickup?
-        morph '.flow-container', render(partial: "steps/pickup/new", locals: { order: @order, pickup: @order.pickup || Pickup.new, message: { content: "Alright, almost done! I just need your flight details to arrange your airport pickup. ", delay: 0 } })
-      else
-        morph '.flow-container', render(partial: "steps/checkout/new", locals: { order: @order, billing: (@order.billing || Billing.new), message: { content: "Thanks #{current_user.first_name}, now please enter your payment details to finalize the order of your Starter Pack", delay: 0 } })
-      end
+    if @shipping.save && @order.update(shipping: @shipping)
+      morph_to_next_step
     else
       morph '.form-base', render(partial: "steps/shipping/form", locals: { shipping: @shipping, order: @order })
+    end
+  end
+
+  def morph_to_next_step
+    if current_user.user_preference.pickup?
+      morph '.flow-container', render(partial: "steps/pickup/new", locals: { order: @order, pickup: @order.pickup || Pickup.new, message: { content: "Alright, almost done! I just need your flight details to arrange your airport pickup. ", delay: 0 } })
+    else
+      morph '.flow-container', render(partial: "steps/checkout/new", locals: { order: @order, billing: (@order.billing || Billing.new), message: { content: "Thanks #{current_user.first_name}, now please enter your payment details to finalize the order of your Starter Pack", delay: 0 } })
     end
   end
 
