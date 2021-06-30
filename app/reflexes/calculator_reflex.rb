25a922362cc9f44da9ad32490fef94a0ca632502
Yoni Touboul <yonitouboul@hotmail.com>
2021-06-30 17:40:28 +0200

    Lint + Clean simulator

--- app/reflexes/calculator_reflex.rb
+++ app/reflexes/calculator_reflex.rb
@@ -1,6 +1,5 @@
 class CalculatorReflex < ApplicationReflex
-
-  def updateCountry
-    morph '.checkboxes-banner', render(partial: "shared/components/checkboxes", locals: {country: Country.find_by(code: element.values[0])})
+  def update_country
+    morph '.checkboxes-banner', render(partial: "shared/components/checkboxes", locals: { country: Country.find_by(code: element.values[0]) })
   end
 end
