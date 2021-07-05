6c54d4e6cae2141712e8c6d11bcaee1427ffaee5
jordan demmouche <jordand75020@hotmail.fr>
2021-07-05 18:19:38 +0200

    Add undiscouted_price striked to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_135808) do
+ActiveRecord::Schema.define(version: 2021_07_05_160858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,6 +142,7 @@ ActiveRecord::Schema.define(version: 2021_07_02_135808) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "country_id", null: false
     t.string "image_url"
+    t.float "undiscounted_price"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["company_id"], name: "index_products_on_company_id"
     t.index ["country_id"], name: "index_products_on_country_id"
