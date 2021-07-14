bd8a17bf72eb8e3907ee554e28304eb4619d8714
jordan demmouche <jordand75020@hotmail.fr>
2021-07-14 12:54:12 +0200

    Separate method in jobs

--- app/jobs/order_marketing_emails_job.rb
+++ app/jobs/order_marketing_emails_job.rb
@@ -3,7 +3,8 @@ class OrderMarketingEmailsJob < ApplicationJob
 
   def perform(*args)
     manage_orders
-    manage_emails
+    send_retarget
+    send_last_call
   end
 
   def manage_orders
@@ -12,14 +13,17 @@ class OrderMarketingEmailsJob < ApplicationJob
     end
   end
 
-  def manage_emails
-    OrderMarketing.where(title: 'orders_sequence', step: 'retarget').each do |marketing|
-      OrderMarketingMailer.with(user: marketing.order.user).retarget.deliver_later
-      marketing.update(step: 'last_call')
-    end
+  def last_call
     OrderMarketing.where(title: 'orders_sequence', step: 'last_call', sent: false).where('created_at < ?', 24.hours.ago).each do |marketing|
       OrderMarketingMailer.with(user: marketing.order.user).last_call.deliver_later
       marketing.update(sent: true)
+    end
+  end
+
+  def send_retarget
+    OrderMarketing.where(title: 'orders_sequence', step: 'retarget').each do |marketing|
+      OrderMarketingMailer.with(user: marketing.order.user).retarget.deliver_later
+      marketing.update(step: 'last_call')
     end
   end
 end
--- app/jobs/user_marketing_emails_job.rb
+++ app/jobs/user_marketing_emails_job.rb
@@ -3,7 +3,9 @@ class UserMarketingEmailsJob < ApplicationJob
 
   def perform(*args)
     manage_users
-    manage_emails
+    retarget
+    second_call
+    last_call
   end
 
   def manage_users
@@ -12,18 +14,25 @@ class UserMarketingEmailsJob < ApplicationJob
     end
   end
 
-  def manage_emails
+  def retarget
     UserMarketing.where(title: 'users_sequence', step: 'retarget').each do |marketing|
       UserMarketingMailer.with(user: marketing.user).retarget.deliver_later
       marketing.update(step: 'second_call')
     end
+  end
+
+  def second_call
     UserMarketing.where(title: 'users_sequence', step: 'second_call').where('created_at < ?',72.hours.ago).each do |marketing|
       UserMarketingMailer.with(user: marketing.user).second_call.deliver_later
       marketing.update(step: 'last_call')
     end
+  end
+
+  def last_call
     UserMarketing.where(title: 'users_sequence', step: 'last_call', sent: false).where('created_at < ?', 72.hours.ago).each do |marketing|
       UserMarketingMailer.with(user: marketing.user).second_call.deliver_later
       marketing.update(sent: true)
     end
   end
+
 end
