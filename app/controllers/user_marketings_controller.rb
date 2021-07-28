17ef723f557ed775e0629717d9e2b2d70d7a9094
Dorian Guitel <dorian.guitel@gmail.com>
2021-07-28 16:21:58 +0200

    finetuned unsubscribe method and texts, added right links to emails, deleted 'unsubscribe' link on welcome email

--- app/controllers/user_marketings_controller.rb
+++ app/controllers/user_marketings_controller.rb
@@ -10,7 +10,7 @@ class UserMarketingsController < ApplicationController
   def unsubscribe
     user_marketing = UserMarketing.find_by(user: (Base64.decode64(params[:user_id]).to_i))
     if user_marketing
-      user_marketing.update(subscribed: false) ? flash[:notice] = 'Successfully unsubscribed' : flash[:alert] = 'An error has occured please try again later...'
+      user_marketing.update(subscribed: false) ? flash[:notice] = 'Successfully unsubscribed' : flash[:alert] = 'An error has occured, please try again later...'
     end
     redirect_to root_path
   end
