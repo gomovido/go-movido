cdee60c2dad8de1a22393b9fc03fa579c12907d2
Dorian Guitel <dorian.guitel@gmail.com>
2021-07-30 16:11:32 +0200

    finetuned legals page with animated + and -

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,6 +4,7 @@ class SubscriptionsController < ApplicationController
     if @order
       @subscription = @order.subscription || Subscription.new
       @message = { content: "This is legals stuff", delay: 0 }
+    else
       flash[:alert] = "You can't access this order."
       redirect_to root_path
     end
