d6524868bb905821fbd0a8374c8fd1184bee4864
jordan demmouche <jordand75020@hotmail.fr>
2021-08-04 15:05:58 +0200

    Homepage & Pricing pages update

--- app/javascript/controllers/logo_controller.js
+++ app/javascript/controllers/logo_controller.js
@@ -4,29 +4,13 @@ export default class extends Controller {
   static targets = [ "word", 'count' ]
 
   connect() {
-    setInterval(this.updateTitle.bind(this), 2000);
+    setInterval(this.updateTitle.bind(this), 780);
   }
 
   updateTitle() {
     let index = [1 + (+this.countTarget.value)]
     if (index >= 6) { index = 0 }
-    this.typeSentence(['Public Transportation?','Housing?', 'Sim Card?', 'Wifi at home?', 'Airport Pickup?', 'Gaz?', 'Energy?'][index])
+    this.wordTarget.innerText = ['Housing?', 'Local Sim card?', 'WiFi?', "Utilities?", 'Public transportation?', 'Airport Pickup?'][index]
     this.countTarget.value = index
-  }
-
-  async typeSentence(sentence, delay = 70) {
-    const letters = sentence.split("");
-    this.wordTarget.innerText = ''
-    let i = 0;
-    while(i < letters.length) {
-      await this.waitForMs(delay);
-      this.wordTarget.append(letters[i]);
-      i++
-    }
-    return;
-  }
-
-  waitForMs(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms))
   }
 };
