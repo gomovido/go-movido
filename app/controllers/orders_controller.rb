54d997ff8c14bd280d4e0aad24bd66516e7301c0
Dorian Guitel <dorian.guitel@gmail.com>
2021-08-23 16:46:20 +0200

    redirection if url is called without '.pdf' (redirects to pdf), two separate layouts for starter pack and settle in invoices

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -24,9 +24,11 @@ class OrdersController < ApplicationController
   def invoice
     @order = Order.find_by(user: current_user, id: params[:order_id])
     respond_to do |format|
-      format.html
+      format.html do
+        redirect_to invoice_path(order_id: @order.id, format: :pdf)
+      end
       format.pdf do
-        render template: "layouts/invoice.html.erb",
+        render template: "layouts/invoice_#{@order.pack == "starter" ? "starter_pack" : "settle_in_pack"}.html.erb",
           pdf: "Movido_#{@order.pack == "starter" ? "Starter" : "Settle_In"}_Pack_#{current_user.first_name}_#{current_user.last_name}_#{@order.created_at.strftime("%Y_%m_%d")}"
       end
     end
