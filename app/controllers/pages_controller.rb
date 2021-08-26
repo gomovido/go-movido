2b32063a76174ec673d32a5cdabbf74ff9d9d41f
jordan demmouche <jordand75020@hotmail.fr>
2021-08-26 17:45:25 +0200

    refactor dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -200,9 +200,4 @@ class PagesController < ApplicationController
     @companies_logo_urls = ['https://res.cloudinary.com/go-movido-com/image/upload/v1615298087/Company%20logos/revolut_sito9h.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621530529/Company%20logos/ee_dx8c5s.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1615298088/Company%20logos/transferwise_uauj31.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1625068211/Company%20logos/bouygues_ggq0hx.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621532485/Company%20logos/british-telecom_k4rxfr.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621530529/Company%20logos/luko_v2mnki.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1615298089/Company%20logos/uniplaces_f1xg2s.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621530813/Company%20logos/uniacco_k1prjs.png', 'https://res.cloudinary.com/go-movido-com/image/upload/v1621530529/Company%20logos/garant-me_xswnji.png']
   end
 
-  def new_dashboard
-    orders = current_user.orders.includes([:products], [:items], [:user], [:shipping], [:subscription])
-    @live_orders = orders.reject { |o| (o.pack == 'settle_in' and o.subscription.nil?) || o.items.blank? }
-    @pending_orders = orders.filter { |o| o.subscription.nil? }
-  end
 end
