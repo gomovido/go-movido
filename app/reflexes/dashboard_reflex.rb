f1be3062e924a9021ae9a03b5be531807e0de21a
jordan demmouche <jordand75020@hotmail.fr>
2021-08-27 16:12:36 +0200

    remove useless method

--- app/reflexes/dashboard_reflex.rb
+++ app/reflexes/dashboard_reflex.rb
@@ -2,7 +2,5 @@ class DashboardReflex < ApplicationReflex
   delegate :current_user, to: :connection
 
   def subscriptions(arg)
-    #@subscription = arg['data']
-    #morph '.products-wrapper', render(partial: 'dashboards/orders/cards/products', locals: { products: @products })
   end
 end
