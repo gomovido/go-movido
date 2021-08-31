5a878a20977e99ccfa0796c9ae2df750190827c9
jordan demmouche <jordand75020@hotmail.fr>
2021-09-01 00:33:57 +0200

    User are hable to change payment details in dashboard

--- app/reflexes/dashboard_reflex.rb
+++ app/reflexes/dashboard_reflex.rb
@@ -4,6 +4,6 @@ class DashboardReflex < ApplicationReflex
   def payment_details(arg)
     @cards = arg['sources']['data']
     @default_source = arg['default_source']
-    morph '.cards-wrapper', render(partial: "dashboards/users/cards", locals: { cards: @cards, default_source: @default_source } )
+    morph '.cards-wrapper', render(partial: "payment_details/cards", locals: { cards: @cards, default_source: @default_source } )
   end
 end
