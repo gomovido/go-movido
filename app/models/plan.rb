e32f9fdf75b2bb9f2748269579b8b3a600bdca9f
jordan demmouche <jordand75020@hotmail.fr>
2021-09-01 15:59:19 +0200

    Operational admin are able to cancel subscription through forest admin

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,12 @@
 class Plan < ApplicationRecord
   belongs_to :subscription
+  belongs_to :product
+  validates :state, :name, presence: true
+  validates :state, inclusion: { in: ["cancelled", "pending_payment", "active"] }
+  after_save :update_subscription_plan
+
+  def update_subscription_plan
+    StripeApiBillingService.new(subscription_id: subscription.id, plan_id: self.id).update_subscription_plan if state == 'cancelled'
+  end
+
 end
--- app/services/stripe_api_billing_service.rb
+++ app/services/stripe_api_billing_service.rb
@@ -3,6 +3,7 @@ class StripeApiBillingService
     @order_id = params[:order_id]
     @stripe_token = params[:stripe_token]
     @subscription_id = params[:subscription_id]
+    @plan_id = params[:plan_id]
   end
 
   def cancel_subscription
@@ -118,6 +119,48 @@ class StripeApiBillingService
       end
     end
     response
+  end
+
+  def update_subscription_plan
+    plan = Plan.find(@plan_id)
+    if plan.stripe_id
+      response = retrieve_subscription
+      if response[:subscription]
+        subscription = response[:subscription].items.data.filter{|a| a.plan.id == plan.stripe_id}[0]
+        subscription_item_id = subscription.id if subscription
+        delete_subscription_item(subscription_item_id) if subscription_item_id
+        delete_service_from_subscription
+      else
+        delete_service_from_subscription
+      end
+    else
+      delete_service_from_subscription
+    end
+  end
+
+  def delete_service_from_subscription
+    subscription = Subscription.find(@subscription_id)
+    plan = Plan.find(@plan_id)
+    plan.product.items.select{|i| i.cart == plan.subscription.order.cart }[0].destroy
+  end
+
+  def delete_subscription_item(subscription_item_id)
+    begin
+      subscription_item = Stripe::SubscriptionItem.delete( subscription_item_id )
+      { subscription_item: subscription_item, error: nil }
+    rescue Stripe::StripeError => e
+      { subscription_item: nil, error: e }
+    end
+  end
+
+  def retrieve_subscription
+    subscription_stripe_id = Subscription.find(@subscription_id).stripe_id
+    begin
+      subscription = Stripe::Subscription.retrieve( subscription_stripe_id.to_s )
+      { subscription: subscription, error: nil }
+    rescue Stripe::StripeError => e
+      { subscription: nil, error: e }
+    end
   end
 
   def update_customer(stripe_id)
