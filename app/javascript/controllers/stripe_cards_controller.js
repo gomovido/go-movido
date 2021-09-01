74f525ff349eba06340962b33511a00743f6667e
jordan demmouche <jordand75020@hotmail.fr>
2021-09-01 11:36:15 +0200

    Remove console.log for production merge

--- app/javascript/controllers/stripe_cards_controller.js
+++ app/javascript/controllers/stripe_cards_controller.js
@@ -59,7 +59,6 @@ export default class extends Controller {
   async fetchStripeCustomer(stripeUrl, secretKey, stripeId) {
     let customer;
     const apiKey = "Bearer " + secretKey
-    console.log(apiKey);
     try {
       customer = await fetch((stripeUrl + "customers/" + stripeId + "?expand[]=sources"), {
         method: "get",
