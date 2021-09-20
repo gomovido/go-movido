07355c35f42d3b5994f85fcd959fb5ae7d22fdb9
jordan demmouche <jordand75020@hotmail.fr>
2021-09-20 12:52:45 +0200

    Delete plan in subscription and flow in dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,14 +2,11 @@ class DashboardsController < ApplicationController
 
   def index
     pending_order = current_user.orders.select{|o| o.state == 'pending_payment'}[0]
-    return redirect_to new_cart_path("settle_in") if current_user.pending_orders? && pending_order.subscription.nil?
+    return redirect_to new_cart_path(pending_order.pack) if current_user.pending_orders? && pending_order.subscription.nil?
     return redirect_to checkout_path(pending_order) if current_user.pending_orders?
     orders = current_user.orders.includes([:products], [:items], [:user], [:shipping], [:subscription])
     @order = current_user.orders.filter{|o| o&.subscription&.state == 'active'}[0]
-    if @order.nil?
-      flash[:alert] = 'You need an active subscription to access dashboard'
-      return redirect_to dashboard_orders_path
-    end
+    return redirect_to dashboard_orders_path if @order.nil?
     @inactive_products = Product.where.not(id: @order.products.pluck(:id)).filter{|p| p.pack.name == 'settle_in' && p.country == current_user.house.country}
   end
 
@@ -25,10 +22,10 @@ class DashboardsController < ApplicationController
 
   def cancel_plan
     response = StripeApiBillingService.new(plan_id: params[:plan_id]).update_subscription_plan
-    if response
+    if response[:error].nil?
       flash[:success] = 'Plan was succesfully cancelled.'
     else
-      flash[:alert] = 'Something went wrong, please try again later.'
+      flash[:alert] = response[:error].message
     end
     redirect_to dashboard_path
   end
